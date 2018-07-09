72299638f9976abac55a822f5eb6c164e38131ec
Adam Price <komidore64@gmail.com>
2018-07-08 21:58:11 -0400

    import movelists

---
+++ src/data/games/guilty_gear_xrd_rev_2/index.js
@@ -0,0 +1,32 @@
+const gg = {
+  name: 'Guilty Gear Xrd REV 2',
+  characters: [
+    require('./answer.json'),
+    require('./axl.json'),
+    require('./baiken.json'),
+    require('./bedman.json'),
+    require('./chipp.json'),
+    require('./dizzy.json'),
+    require('./elphelt.json'),
+    require('./faust.json'),
+    require('./i-no.json'),
+    require('./jack-o.json'),
+    require('./jam.json'),
+    require('./johnny.json'),
+    require('./kum.json'),
+    require('./ky.json'),
+    require('./leo.json'),
+    require('./may.json'),
+    require('./millia.json'),
+    require('./potemkin.json'),
+    require('./ramlethal.json'),
+    require('./raven.json'),
+    require('./sin.json'),
+    require('./slayer.json'),
+    require('./sol.json'),
+    require('./venom.json'),
+    require('./zato.json')
+  ]
+};
+
+export default gg;
---
+++ src/data/games/index.js
@@ -0,0 +1,3 @@
+import guilty_gear_xrd_rev_2 from './guilty_gear_xrd_rev_2';
+
+export const ggxrd = guilty_gear_xrd_rev_2;
