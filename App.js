1568f19d79a9a62f26e2fecdf231a1dc7884b2ee
Adam Price <komidore64@gmail.com>
2018-07-14 18:42:16 -0400

    android was complaining about missing fonts

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import {createStackNavigator} from 'react-navigation';
-import {StyleProvider} from 'native-base';
+import {
+  Container,
+  Content,
+  Spinner,
+  StyleProvider,
+} from 'native-base';
+import {Font} from 'expo';
 
 import getTheme from 'dustup/native-base-theme/components';
 import platform from 'dustup/native-base-theme/variables/platform';
@@ -19,11 +25,40 @@ const RootStack = createStackNavigator(
   {
     initialRouteName: 'GameList',
     headerMode: 'none',
-  }
+  },
 );
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false
+    };
+  }
+
+  componentWillMount() {
+    this.loadFonts();
+  }
+
+  async loadFonts() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+    this.setState({isReady:true});
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <Container>
+          <Content>
+            <Spinner/>
+          </Content>
+        </Container>
+      );
+    }
     return (
       <StyleProvider style={getTheme(platform)}>
         <RootStack/>
