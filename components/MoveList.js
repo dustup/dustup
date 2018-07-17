df7179c39190475764978893b100862713a9a6e6
Adam Price <komidore64@gmail.com>
2018-07-16 20:46:33 -0400

    extract MoveSection

--- components/MoveList.js
+++ components/MoveList.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-  List,
-  ListItem,
-  Separator,
-  Text,
-} from 'native-base';
+import {List} from 'native-base';
 
-import {MoveSectionItem} from 'dustup/components';
+import {MoveSection} from 'dustup/components';
 
 export default class MoveList extends React.Component {
   render() {
@@ -15,48 +10,16 @@ export default class MoveList extends React.Component {
       navigation,
     } = this.props;
 
-    const normals = moves.normals.map((move) => (
-      <MoveSectionItem
-        move={move}
-        key={JSON.stringify(move)}/>
-    ));
-
-    const specials = moves.specials.map((move) => (
-      <MoveSectionItem
-        move={move}
-        key={JSON.stringify(move)}/>
-    ));
-
-    const overdrives = moves.overdrives.map((move) => (
-      <MoveSectionItem
-        move={move}
-        key={JSON.stringify(move)}/>
-    ));
-
-    const instantKills = moves.instant_kills.map((move) => (
-      <MoveSectionItem
-        move={move}
-        key={JSON.stringify(move)}/>
+    const renderedSections = Object.keys(moves).map((section) => (
+      <MoveSection
+        moves={moves[section]}
+        sectionTitle={section}
+        key={section}/>
     ));
 
     return (
       <List>
-        <Separator bordered>
-          <Text>NORMALS</Text>
-        </Separator>
-        {normals}
-        <Separator bordered>
-          <Text>SPECIALS</Text>
-        </Separator>
-        {specials}
-        <Separator bordered>
-          <Text>OVERDRIVES</Text>
-        </Separator>
-        {overdrives}
-        <Separator bordered>
-          <Text>INSTANT KILLS</Text>
-        </Separator>
-        {instantKills}
+        {renderedSections}
       </List>
     );
   }
--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ export FullScreenSpinner from './FullScreenSpinner';
 export GameList from './GameList';
 export GameListItem from './GameListItem';
 export MoveList from './MoveList';
+export MoveSection from './MoveSection';
 export MoveSectionItem from './MoveSectionItem';
 export SimpleMoveListItem from './SimpleMoveListItem';
