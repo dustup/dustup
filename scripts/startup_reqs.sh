17fb956db217a0258462bf0cf1535558accaf21f
Adam Price <komidore64@gmail.com>
2018-07-23 09:05:01 -0400

    fix alex's dumb macbook

--- scripts/startup_reqs.sh
+++ scripts/startup_reqs.sh
@@ -3,6 +3,10 @@
 # show people what we're doing
 set -x
 
-# watchman wants these set
-sudo sysctl -w fs.inotify.max_user_instances=1024
-sudo sysctl -w fs.inotify.max_user_watches=12288
+# watchman wants these set for linux
+sudo sysctl -w fs.inotify.max_user_instances=1024 || true
+sudo sysctl -w fs.inotify.max_user_watches=12288 || true
+
+# watchman wants these set for osx
+sudo sysctl -w kern.maxfiles=5242880 || true
+sudo sysctl -w kern.maxfilesperproc=524288 || true
