d3d09552dd1b845b6a738578dff3a85d5134901a
Adam Price <komidore64@gmail.com>
2018-08-27 19:16:44 -0400

    unist images - akatsuki normals
    - 5A
    - 2A
    - c.B
    - f.B
    - 2B
    - c.C
    - f.C
    - 2C
    - j.A
    - j.7B
    - j.9B
    - j.7C
    - j.9C

--- data/games/under_night_in_birth_exe_late_st/images/akatsuki/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/akatsuki/index.js
@@ -3,4 +3,48 @@ export default [
     imageId: 'portrait',
     image: require('./portrait.png'),
   },
+  {
+    imageId: '5A',
+    image: require('./5A.png'),
+  },
+  {
+    imageId: '2A',
+    image: require('./2A.png'),
+  },
+  {
+    imageId: 'c_B',
+    image: require('./c_B.png'),
+  },
+  {
+    imageId: 'f_B',
+    image: require('./f_B.png'),
+  },
+  {
+    imageId: '2B',
+    image: require('./2B.png'),
+  },
+  {
+    imageId: 'c_C',
+    image: require('./c_C.png'),
+  },
+  {
+    imageId: 'f_C',
+    image: require('./f_C.png'),
+  },
+  {
+    imageId: '2C',
+    image: require('./2C.png'),
+  },
+  {
+    imageId: 'j_A',
+    image: require('./j_A.png'),
+  },
+  {
+    imageId: 'j_9B',
+    image: require('./j_9B.png'),
+  },
+  {
+    imageId: 'j_9C',
+    image: require('./j_9C.png'),
+  },
 ]
