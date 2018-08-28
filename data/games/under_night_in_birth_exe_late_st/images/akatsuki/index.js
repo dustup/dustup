cc9864efdda25c98fe4db2f3aa023980a28861af
Adam Price <komidore64@gmail.com>
2018-08-28 19:46:26 -0400

    unist images - akatsuki command normals
    - 5AA
    - 2AA
    - helm crusher
    - jaw crusher
    - j_8B
    - tile crusher
    - j_8C
    - skull crusher

--- data/games/under_night_in_birth_exe_late_st/images/akatsuki/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/akatsuki/index.js
@@ -47,4 +47,36 @@ export default [
     imageId: 'j_9C',
     image: require('./j_9C.png'),
   },
+  {
+    imageId: '5AA',
+    image: require('./5AA.png'),
+  },
+  {
+    imageId: '2AA',
+    image: require('./2AA.png'),
+  },
+  {
+    imageId: 'helm_crusher',
+    image: require('./helm_crusher.png'),
+  },
+  {
+    imageId: 'jaw_crusher',
+    image: require('./jaw_crusher.png'),
+  },
+  {
+    imageId: 'j_8B',
+    image: require('./j_8B.png'),
+  },
+  {
+    imageId: 'tile_crusher',
+    image: require('./tile_crusher.png'),
+  },
+  {
+    imageId: 'j_8C',
+    image: require('./j_8C.png'),
+  },
+  {
+    imageId: 'skull_crusher',
+    image: require('./skull_crusher.png'),
+  },
 ]
