03ba4f995d5162c0f376c9a9b081dc286d9b1a42
Adam Price <komidore64@gmail.com>
2018-08-29 12:58:02 -0400

    unist images - byakuya normals
    - 5A
    - 2A
    - 5B
    - 2B
    - 5C
    - 2C
    - j_A
    - j_B
    - j_C

--- data/games/under_night_in_birth_exe_late_st/images/byakuya/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/byakuya/index.js
@@ -3,4 +3,40 @@ export default [
     imageId: 'portrait',
     image: require('./portrait.png'),
   },
+  {
+    imageId: '5A',
+    image: require('./5A.png'),
+  },
+  {
+    imageId: '2A',
+    image: require('./2A.png'),
+  },
+  {
+    imageId: '5B',
+    image: require('./5B.png'),
+  },
+  {
+    imageId: '2B',
+    image: require('./2B.png'),
+  },
+  {
+    imageId: '5C',
+    image: require('./5C.png'),
+  },
+  {
+    imageId: '2C',
+    image: require('./2C.png'),
+  },
+  {
+    imageId: 'j_A',
+    image: require('./j_A.png'),
+  },
+  {
+    imageId: 'j_B',
+    image: require('./j_B.png'),
+  },
+  {
+    imageId: 'j_C',
+    image: require('./j_C.png'),
+  },
 ]
