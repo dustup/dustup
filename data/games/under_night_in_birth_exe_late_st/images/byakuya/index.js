3be49d3b799a87695fe141b523ecf6fdd64aa8a7
Adam Price <komidore64@gmail.com>
2018-08-30 18:11:10 -0400

    unist images - byakuya universals, specials, supers
    - force_function
    - force_function_charge
    - throw
    - guard_thrust
    - veil_off
    - how shall i cook you?
    - ...minced?
    - or...shredded?
    - i'll plant it somewhere over here
    - i'll plant it somewhere over here (air)
    - no point in running
    - caught you (a version)
    - caught you (b version)
    - caught you (c version)
    - caught you (d version)
    - you're almost ready to eat
    - you're almost ready to eat (feint)
    - become a part of me
    - endless nightmare

--- data/games/under_night_in_birth_exe_late_st/images/byakuya/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/byakuya/index.js
@@ -63,4 +63,72 @@ export default [
     imageId: '66C',
     image: require('./66C.png'),
   },
+  {
+    imageId: 'force_function',
+    image: require('./force_function.png'),
+  },
+  {
+    imageId: 'force_function_charge',
+    image: require('./force_function_charge.png'),
+  },
+  {
+    imageId: 'throw',
+    image: require('./throw.png'),
+  },
+  {
+    imageId: 'veil_off',
+    image: require('./veil_off.png'),
+  },
+  {
+    imageId: 'how_shall_i_cook_you',
+    image: require('./how_shall_i_cook_you.png'),
+  },
+  {
+    imageId: 'minced',
+    image: require('./minced.png'),
+  },
+  {
+    imageId: 'or_shredded',
+    image: require('./or_shredded.png'),
+  },
+  {
+    imageId: 'i_ll_plant_it_somewhere_over_here',
+    image: require('./i_ll_plant_it_somewhere_over_here.png'),
+  },
+  {
+    imageId: 'i_ll_plant_it_somewhere_over_here_air',
+    image: require('./i_ll_plant_it_somewhere_over_here_air.png'),
+  },
+  {
+    imageId: 'no_point_in_running',
+    image: require('./no_point_in_running.png'),
+  },
+//  {
+//    imageId: 'caught_you_b',
+//    image: require('./caught_you_b.png'),
+//  },
+//  {
+//    imageId: 'caught_you_c',
+//    image: require('./caught_you_c.png'),
+//  },
+//  {
+//    imageId: 'caught_you_d',
+//    image: require('./caught_you_d.png'),
+//  },
+  {
+    imageId: 'you_re_almost_ready_to_eat',
+    image: require('./you_re_almost_ready_to_eat.png'),
+  },
+  {
+    imageId: 'you_re_almost_ready_to_eat_feint',
+    image: require('./you_re_almost_ready_to_eat_feint.png'),
+  },
+  {
+    imageId: 'become_a_part_of_me',
+    image: require('./become_a_part_of_me.png'),
+  },
+  {
+    imageId: 'endless_nightmare',
+    image: require('./endless_nightmare.png'),
+  },
 ]
