a63a50837af9b67c3d2443ac8ec335fc29b40b08
Alex Chvatal <yith@yuggoth.space>
2018-09-15 19:37:52 -0400

    add images for linne from unist

--- data/games/under_night_in_birth_exe_late_st/images/linne/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/linne/index.js
@@ -1,6 +1,114 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "2AA",
+    "image": require('./2AA.png')
+  },
+  {
+    "imageId": "6B",
+    "image": require('./6B.png')
+  },
+  {
+    "imageId": "5CC",
+    "image": require('./5CC.png')
+  },
+  {
+    "imageId": "j6B",
+    "image": require('./j6B.png')
+  },
+  {
+    "imageId": "j2B",
+    "image": require('./j2B.png')
+  },
+  {
+    "imageId": "Wolf_Howl_1",
+    "image": require('./Wolf_Howl_1.png')
+  },
+  {
+    "imageId": "Wolf_Howl_2",
+    "image": require('./Wolf_Howl_2.png')
+  },
+  {
+    "imageId": "Wolf_Howl_3",
+    "image": require('./Wolf_Howl_3.png')
+  },
+  {
+    "imageId": "66C",
+    "image": require('./66C.png')
+  },
+  {
+    "imageId": "Elusive_Flash",
+    "image": require('./Elusive_Flash.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  },
+  {
+    "imageId": "Divine_Chains",
+    "image": require('./Divine_Chains.png')
+  },
+  {
+    "imageId": "Sky_Fangs",
+    "image": require('./Sky_Fangs.png')
+  },
+  {
+    "imageId": "Moon_Gyre",
+    "image": require('./Moon_Gyre.png')
+  },
+  {
+    "imageId": "Tenacious_Mist",
+    "image": require('./Tenacious_Mist.png')
+  },
+  {
+    "imageId": "Flying_Swallow",
+    "image": require('./Flying_Swallow.png')
+  },
+  {
+    "imageId": "The_Diviner",
+    "image": require('./The_Diviner.png')
+  },
+  {
+    "imageId": "Divine_Blaze",
+    "image": require('./Divine_Blaze.png')
+  }
 ]
