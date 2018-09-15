ddc1900ab4b8c2d50017031277aae4985ad5ab00
Adam Price <komidore64@gmail.com>
2018-09-15 16:53:40 -0400

    unist images - eltnum (#95)

--- data/games/under_night_in_birth_exe_late_st/images/eltnum/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/eltnum/index.js
@@ -1,6 +1,130 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+//  {
+//    imageId: 'portrait',
+//    image: require('./portrait.png'),
+//  },
+  {
+    imageId: '5A',
+    image: require('./5A.png'),
+  },
+  {
+    imageId: '2A',
+    image: require('./2A.png'),
+  },
+  {
+    imageId: '5B',
+    image: require('./5B.png'),
+  },
+  {
+    imageId: '2B',
+    image: require('./2B.png'),
+  },
+  {
+    imageId: '5C',
+    image: require('./5C.png'),
+  },
+  {
+    imageId: '2C_1',
+    image: require('./2C_1.png'),
+  },
+//  {
+//    imageId: '2C_2",
+//    image: require('./2C_2.png'),
+//  },
+  {
+    imageId: 'j_A',
+    image: require('./j_A.png'),
+  },
+  {
+    imageId: 'j_B',
+    image: require('./j_B.png'),
+  },
+  {
+    imageId: 'j_C',
+    image: require('./j_C.png'),
+  },
+  {
+    imageId: '6B',
+    image: require('./6B.png'),
+  },
+  {
+    imageId: '5CC',
+    image: require('./5CC.png'),
+  },
+  {
+    imageId: '3C',
+    image: require('./3C.png'),
+  },
+  {
+    imageId: 'j_2B',
+    image: require('./j_2B.png'),
+  },
+  {
+    imageId: '66B',
+    image: require('./66B.png'),
+  },
+  {
+    imageId: '66C',
+    image: require('./66C.png'),
+  },
+  {
+    imageId: 'force_function',
+    image: require('./force_function.png'),
+  },
+  {
+    imageId: 'force_function_charge',
+    image: require('./force_function_charge.png'),
+  },
+  {
+    imageId: 'throw',
+    image: require('./throw.png'),
+  },
+  {
+    imageId: 'throw_air',
+    image: require('./throw_air.png'),
+  },
+  {
+    imageId: 'veil_off',
+    image: require('./veil_off.png'),
+  },
+  {
+    imageId: 'hollow_point',
+    image: require('./hollow_point.png'),
+  },
+  {
+    imageId: 'warning_shot',
+    image: require('./warning_shot.png'),
+  },
+  {
+    imageId: 'reload',
+    image: require('./reload.png'),
+  },
+  {
+    imageId: 'etherite_air',
+    image: require('./etherite_air.png'),
+  },
+  {
+    imageId: 'etherite_ground',
+    image: require('./etherite_ground.png'),
+  },
+  {
+    imageId: 'cutting_sync_1',
+    image: require('./cutting_sync_1.png'),
+  },
+//  {
+//    imageId: 'cutting_sync_2',
+//    image: require('./cutting_sync_2.png'),
+//  },
+  {
+    imageId: 'slide_air',
+    image: require('./slide_air.png'),
+  },
+  {
+    imageId: 'infinite_worth',
+    image: require('./infinite_worth.png'),
+  },
+  {
+    imageId: 'infinite_worth_exs',
+    image: require('./infinite_worth_exs.png'),
+  },
 ]
