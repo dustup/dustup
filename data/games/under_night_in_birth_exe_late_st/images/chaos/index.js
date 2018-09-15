d1ad7616b36b45623181b58065c6dc7d2de01a6e
Adam Price <komidore64@gmail.com>
2018-09-15 12:00:59 -0400

    unist images - chaos

--- data/games/under_night_in_birth_exe_late_st/images/chaos/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/chaos/index.js
@@ -1,6 +1,110 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    imageId: 'portrait',
+    image: require('./portrait.png'),
+  },
+  {
+    imageId: '5A',
+    image: require('./5A.png'),
+  },
+  {
+    imageId: '2A',
+    image: require('./2A.png'),
+  },
+  {
+    imageId: '5B',
+    image: require('./5B.png'),
+  },
+  {
+    imageId: '2B',
+    image: require('./2B.png'),
+  },
+  {
+    imageId: '5C',
+    image: require('./5C.png'),
+  },
+  {
+    imageId: '2C',
+    image: require('./2C.png'),
+  },
+  {
+    imageId: 'j_A',
+    image: require('./j_A.png'),
+  },
+  {
+    imageId: 'j_B',
+    image: require('./j_B.png'),
+  },
+  {
+    imageId: 'j_C',
+    image: require('./j_C.png'),
+  },
+  {
+    imageId: '5BB',
+    image: require('./5BB.png'),
+  },
+  {
+    imageId: '5CC',
+    image: require('./5CC.png'),
+  },
+  {
+    imageId: 'cold_reflection',
+    image: require('./cold_reflection.png'),
+  },
+  {
+    imageId: 'j_2C',
+    image: require('./j_2C.png'),
+  },
+  {
+    imageId: '66C',
+    image: require('./66C.png'),
+  },
+  {
+    imageId: 'force_function',
+    image: require('./force_function.png'),
+  },
+  {
+    imageId: 'throw',
+    image: require('./throw.png'),
+  },
+  {
+    imageId: 'veil_off',
+    image: require('./veil_off.png'),
+  },
+  {
+    imageId: 'prey_in_sight_a',
+    image: require('./prey_in_sight_a.png'),
+  },
+//  {
+//    imageId: 'prey_in_sight_b',
+//    image: require('./prey_in_sight_b.png'),
+//  },
+//  {
+//    imageId: 'prey_in_sight_c',
+//    image: require('./prey_in_sight_c.png'),
+//  },
+  {
+    imageId: 'touch_of_death',
+    image: require('./touch_of_death.png'),
+  },
+  {
+    imageId: 'repel',
+    image: require('./repel.png'),
+  },
+  {
+    imageId: 'concealment',
+    image: require('./concealment.png'),
+  },
+  {
+    imageId: 'slither',
+    image: require('./slither.png'),
+  },
+  {
+    imageId: 'infinite_worth',
+    image: require('./infinite_worth.png'),
+  },
+  {
+    imageId: 'infinite_worth_exs',
+    image: require('./infinite_worth_exs.png'),
+  },
 ]
