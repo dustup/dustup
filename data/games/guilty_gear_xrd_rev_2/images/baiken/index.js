efe1ad911fa3adefd2cd3c280193e18c514e7757
Alex Chvatal <yith@yuggoth.space>
2018-09-14 20:54:06 -0400

    add baiken images (#88)
    * add baiken images
    
    * add image for air azami

--- data/games/guilty_gear_xrd_rev_2/images/baiken/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/baiken/index.js
@@ -1,6 +1,150 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "Tatami_Gaeshi",
+    "image": require('./Tatami_Gaeshi.png')
+  },
+  {
+    "imageId": "Azami",
+    "image": require('./Azami.png')
+  },
+  {
+    "imageId": "Suzuran",
+    "image": require('./Suzuran.png')
+  },
+  {
+    "imageId": "Kuchinashi",
+    "image": require('./Kuchinashi.png')
+  },
+  {
+    "imageId": "Mawarikomi",
+    "image": require('./Mawarikomi.png')
+  },
+  {
+    "imageId": "Sakura",
+    "image": require('./Sakura.png')
+  },
+  {
+    "imageId": "Rokkonsogi",
+    "image": require('./Rokkonsogi.png')
+  },
+  {
+    "imageId": "Yashagatana",
+    "image": require('./Yashagatana.png')
+  },
+  {
+    "imageId": "Air_Azami",
+    "image": require('./Air_Azami.png')
+  },
+  {
+    "imageId": "Tsubaki",
+    "image": require('./Tsubaki.png')
+  },
+  {
+    "imageId": "Kikyou",
+    "image": require('./Kikyou.png')
+  },
+  {
+    "imageId": "Kabari",
+    "image": require('./Kabari.png')
+  },
+  {
+    "imageId": "Himarawi",
+    "image": require('./Himarawi.png')
+  },
+  {
+    "imageId": "Tetsuzansen",
+    "image": require('./Tetsuzansen.png')
+  },
+  {
+    "imageId": "Youzansen",
+    "image": require('./Youzansen.png')
+  },
+  {
+    "imageId": "Tsurane_Zanzuwatashi",
+    "image": require('./Tsurane_Zanzuwatashi.png')
+  },
+  {
+    "imageId": "Metsudo_Kushoudou",
+    "image": require('./Metsudo_Kushoudou.png')
+  },
+  {
+    "imageId": "Garyo_Tensei",
+    "image": require('./Garyo_Tensei.png')
+  }
 ]
