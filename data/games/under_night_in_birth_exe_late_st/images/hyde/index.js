a20959b71b3e591ee9721d5d7b35d8cc8a983bd2
Alex Chvatal <yith@yuggoth.space>
2018-09-15 12:25:30 -0400

    add images for hyde
    for #86

--- data/games/under_night_in_birth_exe_late_st/images/hyde/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/hyde/index.js
@@ -1,6 +1,134 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "5C_charge",
+    "image": require('./5C_charge.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "jC_charge",
+    "image": require('./jC_charge.png')
+  },
+  {
+    "imageId": "6B",
+    "image": require('./6B.png')
+  },
+  {
+    "imageId": "3B",
+    "image": require('./3B.png')
+  },
+  {
+    "imageId": "3C",
+    "image": require('./3C.png')
+  },
+  {
+    "imageId": "j6C",
+    "image": require('./j6C.png')
+  },
+  {
+    "imageId": "j2C",
+    "image": require('./j2C.png')
+  },
+  {
+    "imageId": "66B",
+    "image": require('./66B.png')
+  },
+  {
+    "imageId": "66C",
+    "image": require('./66C.png')
+  },
+  {
+    "imageId": "Dead-Set_Daze",
+    "image": require('./Dead-Set_Daze.png')
+  },
+  {
+    "imageId": "Dead-Set_Daze_charged",
+    "image": require('./Dead-Set_Daze_charged.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Guard_Thrust",
+    "image": require('./Guard_Thrust.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  },
+  {
+    "imageId": "Black_Orbiter",
+    "image": require('./Black_Orbiter.png')
+  },
+  {
+    "imageId": "Dark_Lotus",
+    "image": require('./Dark_Lotus.png')
+  },
+  {
+    "imageId": "Sinistry",
+    "image": require('./Sinistry.png')
+  },
+  {
+    "imageId": "Red_Craver",
+    "image": require('./Red_Craver.png')
+  },
+  {
+    "imageId": "Vacant_Shift",
+    "image": require('./Vacant_Shift.png')
+  },
+  {
+    "imageId": "Shadow_Shock",
+    "image": require('./Shadow_Shock.png')
+  },
+  {
+    "imageId": "Dark_Lotus_air",
+    "image": require('./Dark_Lotus_air.png')
+  },
+  {
+    "imageId": "Veil_Effect",
+    "image": require('./Veil_Effect.png')
+  },
+  {
+    "imageId": "Gyro_Vortex",
+    "image": require('./Gyro_Vortex.png')
+  },
+  {
+    "imageId": "Rage",
+    "image": require('./Rage.png')
+  }
 ]
