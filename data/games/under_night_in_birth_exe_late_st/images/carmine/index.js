fc770d210a1d8db5b40d1f7234c684970edc148a
Adam Price <komidore64@gmail.com>
2018-09-14 20:55:14 -0400

    unist images - carmine (#90)

--- data/games/under_night_in_birth_exe_late_st/images/carmine/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/carmine/index.js
@@ -1,6 +1,134 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+//  {
+//    imageId: 'portrait',
+//    image: require('./portrait.png'),
+//  },
+  {
+    imageId: '5A',
+    image: require('./5A.png'),
+  },
+  {
+    imageId: '2A',
+    image: require('./2A.png'),
+  },
+  {
+    imageId: '5B',
+    image: require('./5B.png'),
+  },
+  {
+    imageId: '2B',
+    image: require('./2B.png'),
+  },
+  {
+    imageId: '5C',
+    image: require('./5C.png'),
+  },
+  {
+    imageId: '2C',
+    image: require('./2C.png'),
+  },
+  {
+    imageId: 'j_A',
+    image: require('./j_A.png'),
+  },
+  {
+    imageId: 'j_B',
+    image: require('./j_B.png'),
+  },
+  {
+    imageId: 'j_C',
+    image: require('./j_C.png'),
+  },
+  {
+    imageId: '5BB',
+    image: require('./5BB.png'),
+  },
+  {
+    imageId: '6B',
+    image: require('./6B.png'),
+  },
+  {
+    imageId: '6BB',
+    image: require('./6BB.png'),
+  },
+  {
+    imageId: 'j_6B',
+    image: require('./j_6B.png'),
+  },
+  {
+    imageId: 'j_6BB',
+    image: require('./j_6BB.png'),
+  },
+  {
+    imageId: '3B',
+    image: require('./3B.png'),
+  },
+  {
+    imageId: '6C',
+    image: require('./6C.png'),
+  },
+  {
+    imageId: '6C_charge',
+    image: require('./6C_charge.png'),
+  },
+  {
+    imageId: '3C',
+    image: require('./3C.png'),
+  },
+  {
+    imageId: '3C_charge',
+    image: require('./3C_charge.png'),
+  },
+  {
+    imageId: 'force_function',
+    image: require('./force_function.png'),
+  },
+  {
+    imageId: 'throw',
+    image: require('./throw.png'),
+  },
+  {
+    imageId: 'veil_off',
+    image: require('./veil_off.png'),
+  },
+  {
+    imageId: 'spin',
+    image: require('./spin.png'),
+  },
+  {
+    imageId: 'spin_air',
+    image: require('./spin_air.png'),
+  },
+  {
+    imageId: 'pulverize',
+    image: require('./pulverize.png'),
+  },
+//  {
+//    imageId: 'pulverize_c',
+//    image: require('./pulverize_c.png'),
+//  },
+  {
+    imageId: 'thrust',
+    image: require('./thrust.png'),
+  },
+//  {
+//    imageId: 'thrust_c',
+//    image: require('./thrust_c.png'),
+//  },
+  {
+    imageId: 'launch',
+    image: require('./launch.png'),
+  },
+  {
+    imageId: 'give_me_that',
+    image: require('./give_me_that.png'),
+  },
+  {
+    imageId: 'infinite_worth',
+    image: require('./infinite_worth.png'),
+  },
+  {
+    imageId: 'infinite_worth_exs',
+    image: require('./infinite_worth_exs.png'),
+  }
 ]
