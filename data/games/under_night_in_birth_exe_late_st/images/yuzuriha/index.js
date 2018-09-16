5777d944ada11c2647c4d94bf9742ad3017e1167
Alex Chvatal <yith@yuggoth.space>
2018-09-16 19:55:40 -0400

    add images for yuzu from unist (#102)
    for #86

--- data/games/under_night_in_birth_exe_late_st/images/yuzuriha/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/yuzuriha/index.js
@@ -1,6 +1,126 @@
 export default [
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "Kiri_A",
+    "image": require('./Kiri_A.png')
+  },
   // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  //   "imageId": "Kiri_B",
+  //   "image": require('./Kiri_C.png')
+  // },
+  // {
+  //   "imageId": "Kiri_C",
+  //   "image": require('./Kiri_C.png')
+  // },
+  {
+    "imageId": "4B",
+    "image": require('./4B.png')
+  },
+  {
+    "imageId": "4C",
+    "image": require('./4C.png')
+  },
+  {
+    "imageId": "j2C",
+    "image": require('./j2C.png')
+  },
+  {
+    "imageId": "66C",
+    "image": require('./66C.png')
+  },
+  {
+    "imageId": "Tachi",
+    "image": require('./Tachi.png')
+  },
+  {
+    "imageId": "Tachi_charge",
+    "image": require('./Tachi_charge.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Guard_Thrust",
+    "image": require('./Guard_Thrust.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  },
+  {
+    "imageId": "Saki",
+    "image": require('./Saki.png')
+  },
+  {
+    "imageId": "Yae_Ichirin",
+    "image": require('./Yae_Ichirin.png')
+  },
+  {
+    "imageId": "Over_Here",
+    "image": require('./Over_Here.png')
+  },
+  {
+    "imageId": "Togiri",
+    "image": require('./Togiri.png')
+  },
+  {
+    "imageId": "Shijima",
+    "image": require('./Shijima.png')
+  },
+  {
+    "imageId": "Watamashi",
+    "image": require('./Watamashi.png')
+  },
+  {
+    "imageId": "Hazumi",
+    "image": require('./Hazumi.png')
+  },
+  {
+    "imageId": "Sazanami",
+    "image": require('./Sazanami.png')
+  },
+  {
+    "imageId": "Kashou",
+    "image": require('./Kashou.png')
+  },
+  {
+    "imageId": "Inochi_Kurenai",
+    "image": require('./Inochi_Kurenai.png')
+  }
 ]
