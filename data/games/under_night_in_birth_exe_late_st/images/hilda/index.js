4b2f01c9a4dbd09f18a2d7eda0ef602d21f34223
Alex Chvatal <yith@yuggoth.space>
2018-09-16 10:22:45 -0400

    add images for hilda from unist (#98)
    * add images for hilda from unist
    
    * include image for hilda's 66B
    
    the image used for 5BB is the same as her 66B

--- data/games/under_night_in_birth_exe_late_st/images/hilda/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/hilda/index.js
@@ -1,6 +1,150 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "5C_charge",
+    "image": require('./5C_charge.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "jC_charge",
+    "image": require('./jC_charge.png')
+  },
+  {
+    "imageId": "3B",
+    "image": require('./3B.png')
+  },
+  {
+    "imageId": "5CC",
+    "image": require('./5CC.png')
+  },
+  {
+    "imageId": "5BB",
+    "image": require('./5BB.png')
+  },
+  {
+    "imageId": "3C",
+    "image": require('./3C.png')
+  },
+  {
+    "imageId": "3CC",
+    "image": require('./3CC.png')
+  },
+  {
+    "imageId": "2CC",
+    "image": require('./2CC.png')
+  },
+  {
+    "imageId": "jCC",
+    "image": require('./jCC.png')
+  },
+  {
+    "imageId": "j2C",
+    "image": require('./j2C.png')
+  },
+  {
+    "imageId": "66C",
+    "image": require('./66C.png')
+  },
+  {
+    "imageId": "Memory_Slip",
+    "image": require('./Memory_Slip.png')
+  },
+  {
+    "imageId": "Memory_Slip_charge",
+    "image": require('./Memory_Slip_charge.png')
+  },
+  {
+    "imageId": "Memory_Slip_variant_1",
+    "image": require('./Memory_Slip_variant_1.png')
+  },
+  {
+    "imageId": "Memory_Slip_variant_2",
+    "image": require('./Memory_Slip_variant_2.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Guard_Thrust",
+    "image": require('./Guard_Thrust.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  },
+  {
+    "imageId": "Skewer",
+    "image": require('./Skewer.png')
+  },
+  {
+    "imageId": "Interference",
+    "image": require('./Interference.png')
+  },
+  {
+    "imageId": "Condensity_Gloom",
+    "image": require('./Condensity_Gloom.png')
+  },
+  {
+    "imageId": "Dismal_Sign",
+    "image": require('./Dismal_Sign.png')
+  },
+  {
+    "imageId": "Fallen_Pain",
+    "image": require('./Fallen_Pain.png')
+  },
+  {
+    "imageId": "Pillar_of_Revenance",
+    "image": require('./Pillar_of_Revenance.png')
+  },
+  {
+    "imageId": "Aspersivity",
+    "image": require('./Aspersivity.png')
+  },
+  {
+    "imageId": "Nail_Torrent",
+    "image": require('./Nail_Torrent.png')
+  },
+  {
+    "imageId": "Impalement",
+    "image": require('./Impalement.png')
+  },
+  {
+    "imageId": "Darkness",
+    "image": require('./Darkness.png')
+  }
 ]
