712512da13551126f2ccce1b17a7839ffa3b78f3
Alex Chvatal <achvatal@redhat.com>
2018-09-17 09:56:58 -0400

    add images for orie from unist
    for #86

--- data/games/under_night_in_birth_exe_late_st/images/orie/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/orie/index.js
@@ -1,6 +1,134 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "6B",
+    "image": require('./6B.png')
+  },
+  {
+    "imageId": "3B",
+    "image": require('./3B.png')
+  },
+  {
+    "imageId": "3C",
+    "image": require('./3C.png')
+  },
+  {
+    "imageId": "4C",
+    "image": require('./4C.png')
+  },
+  {
+    "imageId": "j2C",
+    "image": require('./j2C.png')
+  },
+  {
+    "imageId": "66B",
+    "image": require('./66B.png')
+  },
+  {
+    "imageId": "66C",
+    "image": require('./66C.png')
+  },
+  {
+    "imageId": "Aerial_Leap",
+    "image": require('./Aerial_Leap.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  },
+  {
+    "imageId": "Divine_Thrust",
+    "image": require('./Divine_Thrust.png')
+  },
+  {
+    "imageId": "Sacred_Arrow",
+    "image": require('./Sacred_Arrow.png')
+  },
+  {
+    "imageId": "Command_Order_A",
+    "image": require('./Command_Order_A.png')
+  },
+  //{
+  //  "imageId": "Command_Order_B",
+  //  "image": require('./Command_Order_B.png')
+  //},
+  //{
+  //  "imageId": "Command_Order_C",
+  //  "image": require('./Command_Order_C.png')
+  //},
+  {
+    "imageId": "Thick_and_Quick",
+    "image": require('./Thick_and_Quick.png')
+  },
+  {
+    "imageId": "Succession",
+    "image": require('./Succession.png')
+  },
+  {
+    "imageId": "Command_Order_A_air",
+    "image": require('./Command_Order_A_air.png')
+  },
+  //{
+  //  "imageId": "Command_Order_B_air",
+  //  "image": require('./Command_Order_B_air.png')
+  //},
+  //{
+  //  "imageId": "Command_Order_C_air",
+  //  "image": require('./Command_Order_C_air.png')
+  //},
+  {
+    "imageId": "Sealing_Hoplon",
+    "image": require('./Sealing_Hoplon.png')
+  },
+  {
+    "imageId": "Sacred_Spire",
+    "image": require('./Sacred_Spire.png')
+  },
+  {
+    "imageId": "Luminous_Embrace",
+    "image": require('./Luminous_Embrace.png')
+  },
+  {
+    "imageId": "Rest_In_Peace",
+    "image": require('./Rest_In_Peace.png')
+  }
 ]
