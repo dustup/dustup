13bad142d4acb2a77364b89848eae46586353d26
Alex Chvatal <yith@yuggoth.space>
2018-09-17 19:34:12 -0400

    add a couple utility scripts (#107)
    index_images.rb - generates data/games/${game}/images/${character}/index.js
    using the filenames as the imageId
    
    missing_imageid.sh - identifies imageIds from the movespec for a character
    which are not present in the character's images index.js

---
+++ scripts/index_images.rb
@@ -0,0 +1,30 @@
+#!/bin/env ruby
+
+require 'optparse'
+
+def usage
+  puts File.basename(__FILE__) + " GAME-NAME CHARACTER-NAME"
+  exit 1
+end
+
+game = ARGV[0]
+character = ARGV[1]
+
+usage if game.nil? or character.nil?
+
+Dir.chdir(File.dirname(__FILE__) + "/../data/games/#{game}/images/#{character}/")
+
+image_hashes = Dir.glob('*.png').collect do |image|
+"  {
+    \"imageId\": \"#{File.basename(image, File.extname(image))}\",
+    \"image\": require('./#{image}')
+  }"
+end
+
+indices = 
+"export default [
+#{image_hashes.join(",\n")}
+]
+"
+
+File.write('index.js', indices)
