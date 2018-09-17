be74479b96945a6ec0bcbb23ca877c5876276c6f
Alex Chvatal <yith@yuggoth.space>
2018-09-16 21:16:17 -0400

    add images for mika from unist
    for #86

--- data/games/under_night_in_birth_exe_late_st/images/mika/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/mika/index.js
@@ -1,6 +1,118 @@
 export default [
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "5C_charge",
+    "image": require('./5C_charge.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "jC_charge",
+    "image": require('./jC_charge.png')
+  },
+  {
+    "imageId": "5BB",
+    "image": require('./5BB.png')
+  },
+  {
+    "imageId": "3B",
+    "image": require('./3B.png')
+  },
+  {
+    "imageId": "3C",
+    "image": require('./3C.png')
+  },
+  {
+    "imageId": "jAA",
+    "image": require('./jAA.png')
+  },
+  {
+    "imageId": "66B",
+    "image": require('./66B.png')
+  },
+  {
+    "imageId": "66C",
+    "image": require('./66C.png')
+  },
+  {
+    "imageId": "Mikas_Crash",
+    "image": require('./Mikas_Crash.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  },
+  {
+    "imageId": "Mikas_Missile_A",
+    "image": require('./Mikas_Missile_A.png')
+  },
   // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  //   "imageId": "Mikas_Missile_B",
+  //   "image": require('./Mikas_Missile_B.png')
+  // },
+  // {
+  //   "imageId": "Mikas_Missile_B_air",
+  //   "image": require('./Mikas_Missile_B_air.png')
+  // },
+  {
+    "imageId": "Mikas_Tornado",
+    "image": require('./Mikas_Tornado.png')
+  },
+  {
+    "imageId": "Mikas_Twister",
+    "image": require('./Mikas_Twister.png')
+  },
+  {
+    "imageId": "Mikas_Cannon",
+    "image": require('./Mikas_Cannon.png')
+  },
+  {
+    "imageId": "Mikas_Hip_Attack",
+    "image": require('./Mikas_Hip_Attack.png')
+  },
+  {
+    "imageId": "Mikas_Revolution",
+    "image": require('./Mikas_Revolution.png')
+  },
+  {
+    "imageId": "Mikas_Galaxy",
+    "image": require('./Mikas_Galaxy.png')
+  }
 ]
