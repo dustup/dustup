22431f183ef3a713817e805bd9af3ed2d9763f01
Alex Chvatal <achvatal@redhat.com>
2018-09-17 16:42:21 -0400

    add images for seth from unist and tweak his movespec
    for #86

--- data/games/under_night_in_birth_exe_late_st/images/seth/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/seth/index.js
@@ -1,6 +1,118 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "Distant_Frontier",
+    "image": require('./Distant_Frontier.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "j6C",
+    "image": require('./j6C.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "Vanishing_Doubt",
+    "image": require('./Vanishing_Doubt.png')
+  },
+  {
+    "imageId": "j22_6A",
+    "image": require('./j22_6A.png')
+  },
+  {
+    "imageId": "Captive_Segment",
+    "image": require('./Captive_Segment.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "j22",
+    "image": require('./j22.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "Convict",
+    "image": require('./Convict.png')
+  },
+  {
+    "imageId": "Penetrate",
+    "image": require('./Penetrate.png')
+  },
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "6C",
+    "image": require('./6C.png')
+  },
+  {
+    "imageId": "j2C",
+    "image": require('./j2C.png')
+  },
+  {
+    "imageId": "66B",
+    "image": require('./66B.png')
+  },
+  {
+    "imageId": "66C",
+    "image": require('./66C.png')
+  },
+  {
+    "imageId": "Chained_Nefarity",
+    "image": require('./Chained_Nefarity.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "3B",
+    "image": require('./3B.png')
+  },
+  {
+    "imageId": "j44",
+    "image": require('./j44.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "3C",
+    "image": require('./3C.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "Dual_Piercing_Vengeance",
+    "image": require('./Dual_Piercing_Vengeance.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  },
+  {
+    "imageId": "Abyssal_Geometry",
+    "image": require('./Abyssal_Geometry.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  }
 ]
