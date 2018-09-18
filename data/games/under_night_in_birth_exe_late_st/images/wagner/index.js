d50aa6cbea1975a4d8676d8e84e8cfbb3bfa9de1
Alex Chvatal <yith@yuggoth.space>
2018-09-17 20:29:49 -0400

    add images for wagner from unist
    for #86

--- data/games/under_night_in_birth_exe_late_st/images/wagner/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/wagner/index.js
@@ -1,6 +1,134 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "Kugel_Blitz",
+    "image": require('./Kugel_Blitz.png')
+  },
+  {
+    "imageId": "Megiddo_Ausserst_Groesse",
+    "image": require('./Megiddo_Ausserst_Groesse.png')
+  },
+  {
+    "imageId": "6C",
+    "image": require('./6C.png')
+  },
+  {
+    "imageId": "6B",
+    "image": require('./6B.png')
+  },
+  {
+    "imageId": "Schild",
+    "image": require('./Schild.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "Sturm_Schlag_charge",
+    "image": require('./Sturm_Schlag_charge.png')
+  },
+  {
+    "imageId": "66B",
+    "image": require('./66B.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "66C",
+    "image": require('./66C.png')
+  },
+  {
+    "imageId": "5C_charge",
+    "image": require('./5C_charge.png')
+  },
+  {
+    "imageId": "Filthy_Dog",
+    "image": require('./Filthy_Dog.png')
+  },
+  {
+    "imageId": "Sturm_Brecher",
+    "image": require('./Sturm_Brecher.png')
+  },
+  {
+    "imageId": "Hitze_Falke",
+    "image": require('./Hitze_Falke.png')
+  },
+  {
+    "imageId": "Sturm_Brecher_buff_charge",
+    "image": require('./Sturm_Brecher_buff_charge.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  },
+  {
+    "imageId": "3C",
+    "image": require('./3C.png')
+  },
+  {
+    "imageId": "j2C",
+    "image": require('./j2C.png')
+  },
+  {
+    "imageId": "jC_charge",
+    "image": require('./jC_charge.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "Sturm_Brecher_charge",
+    "image": require('./Sturm_Brecher_charge.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "Sturm_Schlag",
+    "image": require('./Sturm_Schlag.png')
+  },
+  {
+    "imageId": "Schild_charge",
+    "image": require('./Schild_charge.png')
+  },
+  {
+    "imageId": "Wackenroder",
+    "image": require('./Wackenroder.png')
+  },
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "Lohen_Schneide",
+    "image": require('./Lohen_Schneide.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  },
+  {
+    "imageId": "Sturm_Brecher_buff",
+    "image": require('./Sturm_Brecher_buff.png')
+  }
 ]
