19a5ab757058c960fa8894c503e664e37f9981ee
Adam Price <komidore64@gmail.com>
2018-09-18 16:37:47 -0400

    unist images - merkava

--- data/games/under_night_in_birth_exe_late_st/images/merkava/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/merkava/index.js
@@ -1,6 +1,126 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+//  {
+//    imageId: 'portrait',
+//    image: require('./portrait.png'),
+//  },
+  {
+    imageId: '5A',
+    image: require('./5A.png'),
+  },
+  {
+    imageId: '2A',
+    image: require('./2A.png'),
+  },
+  {
+    imageId: '5B',
+    image: require('./5B.png'),
+  },
+  {
+    imageId: '2B',
+    image: require('./2B.png'),
+  },
+  {
+    imageId: '5C',
+    image: require('./5C.png'),
+  },
+  {
+    imageId: '2C',
+    image: require('./2C.png'),
+  },
+  {
+    imageId: 'j_A',
+    image: require('./j_A.png'),
+  },
+  {
+    imageId: 'j_B',
+    image: require('./j_B.png'),
+  },
+  {
+    imageId: 'j_C',
+    image: require('./j_C.png'),
+  },
+  {
+    imageId: '5BB',
+    image: require('./5BB.png'),
+  },
+  {
+    imageId: '4B',
+    image: require('./4B.png'),
+  },
+  {
+    imageId: '6C',
+    image: require('./6C.png'),
+  },
+  {
+    imageId: '2CC',
+    image: require('./2CC.png'),
+  },
+  {
+    imageId: '4C',
+    image: require('./4C.png'),
+  },
+  {
+    imageId: '3C',
+    image: require('./3C.png'),
+  },
+  {
+    imageId: '3C_charge',
+    image: require('./3C_charge.png'),
+  },
+  {
+    imageId: 'j_2C',
+    image: require('./j_2C.png'),
+  },
+  {
+    imageId: 'force_function',
+    image: require('./force_function.png'),
+  },
+  {
+    imageId: 'throw',
+    image: require('./throw.png'),
+  },
+  {
+    imageId: 'veil_off',
+    image: require('./veil_off.png'),
+  },
+  {
+    imageId: 'flying_attack',
+    image: require('./flying_attack.png'),
+  },
+  {
+    imageId: 'execute_drill',
+    image: require('./execute_drill.png'),
+  },
+  {
+    imageId: 'execute_pierce',
+    image: require('./execute_pierce.png'),
+  },
+  {
+    imageId: 'execute_seize_and_devour',
+    image: require('./execute_seize_and_devour.png'),
+  },
+//  {
+//    imageId: 'execute_seize_and_devour_b',
+//    image: require('./execute_seize_and_devour_b.png'),
+//  },
+  {
+    imageId: 'execute_breathe',
+    image: require('./execute_breathe.png'),
+  },
+  {
+    imageId: 'execute_coil',
+    image: require('./execute_coil.png'),
+  },
+  {
+    imageId: 'execute_domination',
+    image: require('./execute_domination.png'),
+  },
+  {
+    imageId: 'infinite_worth',
+    image: require('./infinite_worth.png'),
+  },
+  {
+    imageId: 'infinite_worth_exs',
+    image: require('./infinite_worth_exs.png'),
+  },
 ]
