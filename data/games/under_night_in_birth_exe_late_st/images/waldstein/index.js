8a12036a7f8ab4cd3773c8b2d7f4c21dc5018dcd
Alex Chvatal <yith@yuggoth.space>
2018-09-18 16:43:34 -0400

    add images for waldstein from unist (#111)
    for #86

--- data/games/under_night_in_birth_exe_late_st/images/waldstein/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/waldstein/index.js
@@ -1,6 +1,114 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "j6C",
+    "image": require('./j6C.png')
+  },
+  {
+    "imageId": "Durchbohren_A",
+    "image": require('./Durchbohren_A.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "Durchbohren_B",
+    "image": require('./Durchbohren_B.png')
+  },
+  {
+    "imageId": "Verderben",
+    "image": require('./Verderben.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "Katastrophe",
+    "image": require('./Katastrophe.png')
+  },
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "4B",
+    "image": require('./4B.png')
+  },
+  {
+    "imageId": "6C",
+    "image": require('./6C.png')
+  },
+  {
+    "imageId": "j2C",
+    "image": require('./j2C.png')
+  },
+  {
+    "imageId": "66B",
+    "image": require('./66B.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "Werfen_Erschlagen",
+    "image": require('./Werfen_Erschlagen.png')
+  },
+  {
+    "imageId": "Eisen_Nagel",
+    "image": require('./Eisen_Nagel.png')
+  },
+  {
+    "imageId": "1A",
+    "image": require('./1A.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "3C",
+    "image": require('./3C.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "Wirbelwind",
+    "image": require('./Wirbelwind.png')
+  },
+  {
+    "imageId": "Guard_Thrust",
+    "image": require('./Guard_Thrust.png')
+  },
+  {
+    "imageId": "Ferzen_Volf",
+    "image": require('./Ferzen_Volf.png')
+  },
+  {
+    "imageId": "Sturmangriff",
+    "image": require('./Sturmangriff.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  }
 ]
