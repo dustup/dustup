32dccdb83703c51e1c12685084830aca96f858b5
Alex Chvatal <achvatal@redhat.com>
2018-09-19 08:43:58 -0400

    add images for vatista from unist
    for #86

--- data/games/under_night_in_birth_exe_late_st/images/vatista/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/vatista/index.js
@@ -1,6 +1,94 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "Armabellum",
+    "image": require('./Armabellum.png')
+  },
+  {
+    "imageId": "5BB",
+    "image": require('./5BB.png')
+  },
+  {
+    "imageId": "Zahhishio",
+    "image": require('./Zahhishio.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "66B",
+    "image": require('./66B.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "Transvoranse",
+    "image": require('./Transvoranse.png')
+  },
+  {
+    "imageId": "Luvel_Angelus",
+    "image": require('./Luvel_Angelus.png')
+  },
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  },
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "2BB",
+    "image": require('./2BB.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "Lateus_Orbis",
+    "image": require('./Lateus_Orbis.png')
+  },
+  {
+    "imageId": "Stella_Lumen",
+    "image": require('./Stella_Lumen.png')
+  },
+  {
+    "imageId": "Guard_Thrust",
+    "image": require('./Guard_Thrust.png')
+  },
+  {
+    "imageId": "Leviosa",
+    "image": require('./Leviosa.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  },
+  {
+    "imageId": "Mikoruseo",
+    "image": require('./Mikoruseo.png')
+  }
 ]
