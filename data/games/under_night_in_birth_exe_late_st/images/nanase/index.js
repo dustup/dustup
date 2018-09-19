d368c1f873506c975d2045054d2783d19d0e6a96
Alex Chvatal <achvatal@redhat.com>
2018-09-19 10:55:46 -0400

    add images for nanase from unist (and fix typos)
    for #86

--- data/games/under_night_in_birth_exe_late_st/images/nanase/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/nanase/index.js
@@ -1,6 +1,126 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "Halte_Once_in_a_While",
+    "image": require('./Halte_Once_in_a_While.png')
+  },
+  {
+    "imageId": "j2C_charge",
+    "image": require('./j2C_charge.png')
+  },
+  {
+    "imageId": "jC_charge",
+    "image": require('./jC_charge.png')
+  },
+  {
+    "imageId": "2C",
+    "image": require('./2C.png')
+  },
+  {
+    "imageId": "2A",
+    "image": require('./2A.png')
+  },
+  {
+    "imageId": "jB",
+    "image": require('./jB.png')
+  },
+  {
+    "imageId": "66B",
+    "image": require('./66B.png')
+  },
+  {
+    "imageId": "Plumage_Dancing_in_the_Wind",
+    "image": require('./Plumage_Dancing_in_the_Wind.png')
+  },
+  {
+    "imageId": "Breezs_Liberte",
+    "image": require('./Breezs_Liberte.png')
+  },
+  {
+    "imageId": "5C",
+    "image": require('./5C.png')
+  },
+  {
+    "imageId": "Chasing_le_Reve",
+    "image": require('./Chasing_le_Reve.png')
+  },
+  {
+    "imageId": "5C_charge",
+    "image": require('./5C_charge.png')
+  },
+  {
+    "imageId": "Lumiere_of_the_Dawn",
+    "image": require('./Lumiere_of_the_Dawn.png')
+  },
+  {
+    "imageId": "Agnes_Invitation",
+    "image": require('./Agnes_Invitation.png')
+  },
+  {
+    "imageId": "5A",
+    "image": require('./5A.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Let_the_Fleur_Carry_your_Feelings",
+    "image": require('./Let_the_Fleur_Carry_your_Feelings.png')
+  },
+  {
+    "imageId": "5B",
+    "image": require('./5B.png')
+  },
+  {
+    "imageId": "Advancer_With_You",
+    "image": require('./Advancer_With_You.png')
+  },
+  {
+    "imageId": "Aureole_of_the_Starry_Sky",
+    "image": require('./Aureole_of_the_Starry_Sky.png')
+  },
+  {
+    "imageId": "jC",
+    "image": require('./jC.png')
+  },
+  {
+    "imageId": "Conveying_my_Vrai_Coeur",
+    "image": require('./Conveying_my_Vrai_Coeur.png')
+  },
+  {
+    "imageId": "jA",
+    "image": require('./jA.png')
+  },
+  {
+    "imageId": "jBB",
+    "image": require('./jBB.png')
+  },
+  {
+    "imageId": "2B",
+    "image": require('./2B.png')
+  },
+  {
+    "imageId": "66C",
+    "image": require('./66C.png')
+  },
+  {
+    "imageId": "j2C",
+    "image": require('./j2C.png')
+  },
+  {
+    "imageId": "j8C",
+    "image": require('./j8C.png')
+  },
+  {
+    "imageId": "3C",
+    "image": require('./3C.png')
+  },
+  {
+    "imageId": "Atmosphere_of_the_Aether",
+    "image": require('./Atmosphere_of_the_Aether.png')
+  },
+  {
+    "imageId": "Veil_Off",
+    "image": require('./Veil_Off.png')
+  }
 ]
