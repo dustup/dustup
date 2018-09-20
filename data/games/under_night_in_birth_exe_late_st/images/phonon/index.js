0431ebf3b7100ff09d627e99beaa64b17f07e47f
Adam Price <komidore64@gmail.com>
2018-09-19 20:35:10 -0400

    unist images - phonon

--- data/games/under_night_in_birth_exe_late_st/images/phonon/index.js
+++ data/games/under_night_in_birth_exe_late_st/images/phonon/index.js
@@ -1,6 +1,146 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+//  {
+//    imageId: 'portrait',
+//    image: require('./portrait.png'),
+//  },
+  {
+    imageId: '5A',
+    image: require('./5A.png'),
+  },
+  {
+    imageId: '2A',
+    image: require('./2A.png'),
+  },
+  {
+    imageId: '5B',
+    image: require('./5B.png'),
+  },
+  {
+    imageId: '2B',
+    image: require('./2B.png'),
+  },
+  {
+    imageId: '5C_1',
+    image: require('./5C_1.png'),
+  },
+  {
+    imageId: '5C_2',
+    image: require('./5C_2.png'),
+  },
+  {
+    imageId: '2C',
+    image: require('./2C.png'),
+  },
+  {
+    imageId: 'j_A',
+    image: require('./j_A.png'),
+  },
+  {
+    imageId: 'j_B',
+    image: require('./j_B.png'),
+  },
+  {
+    imageId: 'j_C',
+    image: require('./j_C.png'),
+  },
+  {
+    imageId: 'j_C_charge',
+    image: require('./j_C_charge.png'),
+  },
+  {
+    imageId: '5BB',
+    image: require('./5BB.png'),
+  },
+  {
+    imageId: '4B',
+    image: require('./4B.png'),
+  },
+  {
+    imageId: '3C',
+    image: require('./3C.png'),
+  },
+  {
+    imageId: 'j_2B',
+    image: require('./j_2B.png'),
+  },
+  {
+    imageId: 'j_2B_charge',
+    image: require('./j_2B_charge.png'),
+  },
+  {
+    imageId: 'j_4B',
+    image: require('./j_4B.png'),
+  },
+  {
+    imageId: '66B',
+    image: require('./66B.png'),
+  },
+  {
+    imageId: '66C',
+    image: require('./66C.png'),
+  },
+  {
+    imageId: 'force_function',
+    image: require('./force_function.png'),
+  },
+  {
+    imageId: 'throw',
+    image: require('./throw.png'),
+  },
+  {
+    imageId: 'guard_thrust',
+    image: require('./guard_thrust.png'),
+  },
+  {
+    imageId: 'veil_off',
+    image: require('./veil_off.png'),
+  },
+  {
+    imageId: 'affliction',
+    image: require('./affliction.png'),
+  },
+  {
+    imageId: 'affliction_air',
+    image: require('./affliction_air.png'),
+  },
+  {
+    imageId: 'guidance_ascension_a',
+    image: require('./guidance_ascension_a.png'),
+  },
+//  {
+//    imageId: 'guidance_ascension_b',
+//    image: require('./guidance_ascension_b.png'),
+//  },
+//  {
+//    imageId: 'guidance_ascension_c',
+//    image: require('./guidance_ascension_c.png'),
+//  },
+  {
+    imageId: 'rhythmic_satisfaction_1',
+    image: require('./rhythmic_satisfaction_1.png'),
+  },
+//  {
+//    imageId: 'rhythmic_satisfaction_2',
+//    image: require('./rhythmic_satisfaction_2.png'),
+//  },
+//  {
+//    imageId: 'rhythmic_satisfaction_3',
+//    image: require('./rhythmic_satisfaction_3.png'),
+//  },
+  {
+    imageId: 'restraint_a',
+    image: require('./restraint_a.png'),
+  },
+//  {
+//    imageId: 'restraint_b',
+//    image: require('./restraint_b.png'),
+//  },
+  {
+    imageId: 'infinite_worth',
+    image: require('./infinite_worth.png'),
+  },
+  {
+    imageId: 'infinite_worth_exs',
+    image: require('./infinite_worth_exs.png'),
+  },
 ]
