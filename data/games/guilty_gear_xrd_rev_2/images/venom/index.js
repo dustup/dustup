de7bf5b4c5de0bc9bf312f9b1b437c57ec275601
Adam Price <komidore64@gmail.com>
2018-09-21 11:30:59 -0400

    GGXrd-R2 images - venom (#116)

--- data/games/guilty_gear_xrd_rev_2/images/venom/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/venom/index.js
@@ -1,6 +1,114 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    imageId: '5P',
+    image: require('./5P.png'),
+  },
+  {
+    imageId: '5K',
+    image: require('./5K.png'),
+  },
+  {
+    imageId: 'c_S',
+    image: require('./c_S.png'),
+  },
+  {
+    imageId: 'f_S',
+    image: require('./f_S.png'),
+  },
+  {
+    imageId: '5H',
+    image: require('./5H.png'),
+  },
+  {
+    imageId: '5D',
+    image: require('./5D.png'),
+  },
+  {
+    imageId: '2P',
+    image: require('./2P.png'),
+  },
+  {
+    imageId: '2K',
+    image: require('./2K.png'),
+  },
+  {
+    imageId: '2S',
+    image: require('./2S.png'),
+  },
+  {
+    imageId: '2H',
+    image: require('./2H.png'),
+  },
+  {
+    imageId: '2D',
+    image: require('./2D.png'),
+  },
+  {
+    imageId: '6P',
+    image: require('./6P.png'),
+  },
+  {
+    imageId: '6H',
+    image: require('./6H.png'),
+  },
+  {
+    imageId: 'j_P',
+    image: require('./j_P.png'),
+  },
+  {
+    imageId: 'j_K',
+    image: require('./j_K.png'),
+  },
+  {
+    imageId: 'j_S',
+    image: require('./j_S.png'),
+  },
+  {
+    imageId: 'j_H',
+    image: require('./j_H.png'),
+  },
+  {
+    imageId: 'j_D',
+    image: require('./j_D.png'),
+  },
+  {
+    imageId: 'ball_set',
+    image: require('./ball_set.png'),
+  },
+  {
+    imageId: 'qv',
+    image: require('./qv.png'),
+  },
+  {
+    imageId: 'teleport',
+    image: require('./teleport.png'),
+  },
+  {
+    imageId: 'stinger_aim',
+    image: require('./stinger_aim.png'),
+  },
+  {
+    imageId: 'carcass_raid',
+    image: require('./carcass_raid.png'),
+  },
+  {
+    imageId: 'double_head_morbid',
+    image: require('./double_head_morbid.png'),
+  },
+  {
+    imageId: 'mad_struggle',
+    image: require('./mad_struggle.png'),
+  },
+  {
+    imageId: 'dark_angel',
+    image: require('./dark_angel.png'),
+  },
+  {
+    imageId: 'red_hail',
+    image: require('./red_hail.png'),
+  },
+  {
+    imageId: 'bishop_runout',
+    image: require('./bishop_runout.png'),
+  },
 ]
