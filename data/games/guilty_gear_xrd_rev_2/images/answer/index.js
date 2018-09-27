3b6b53ec6ac81c4b1197150e400aa6c0d5d44981
Adam Price <komidore64@gmail.com>
2018-09-27 12:35:17 -0400

    GGXrd-R2 - answer images (#118)
    * GGXrd-R2 - answer images
    
    * seal of approval doesn't match with seal stance cancel

--- data/games/guilty_gear_xrd_rev_2/images/answer/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/answer/index.js
@@ -1,6 +1,146 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    imageId: '5P',
+    image: require('./5P.png'),
+  },
+  {
+    imageId: '5K',
+    image: require('./5K.png'),
+  },
+  {
+    imageId: 'c_S',
+    image: require('./c_S.png'),
+  },
+  {
+    imageId: 'f_S',
+    image: require('./f_S.png'),
+  },
+  {
+    imageId: '5H',
+    image: require('./5H.png'),
+  },
+  {
+    imageId: '5D',
+    image: require('./5D.png'),
+  },
+  {
+    imageId: '6P',
+    image: require('./6P.png'),
+  },
+  {
+    imageId: '6K',
+    image: require('./6K.png'),
+  },
+  {
+    imageId: '6H',
+    image: require('./6H.png'),
+  },
+  {
+    imageId: '2P',
+    image: require('./2P.png'),
+  },
+  {
+    imageId: '2K',
+    image: require('./2K.png'),
+  },
+  {
+    imageId: '2S',
+    image: require('./2S.png'),
+  },
+  {
+    imageId: '2H',
+    image: require('./2H.png'),
+  },
+  {
+    imageId: '2D',
+    image: require('./2D.png'),
+  },
+  {
+    imageId: 'j_P',
+    image: require('./j_P.png'),
+  },
+  {
+    imageId: 'j_K',
+    image: require('./j_K.png'),
+  },
+  {
+    imageId: 'j_S',
+    image: require('./j_S.png'),
+  },
+  {
+    imageId: 'j_H',
+    image: require('./j_H.png'),
+  },
+  {
+    imageId: 'j_D',
+    image: require('./j_D.png'),
+  },
+  {
+    imageId: 'business_ninpo_caltrops',
+    image: require('./business_ninpo_caltrops.png'),
+  },
+  {
+    imageId: 'business_ninpo_under_the_rug',
+    image: require('./business_ninpo_under_the_rug.png'),
+  },
+  {
+    imageId: 'business_ninpo_under_the_bus',
+    image: require('./business_ninpo_under_the_bus.png'),
+  },
+  {
+    imageId: 'savvy_ninpo_request_for_approval',
+    image: require('./savvy_ninpo_request_for_approval.png'),
+  },
+//  {
+//    imageId: 'savvy_ninpo_seal_of_approval',
+//    image: require('./savvy_ninpo_seal_of_approval.png'),
+//  },
+  {
+    imageId: 'savvy_ninpo_data_logging',
+    image: require('./savvy_ninpo_data_logging.png'),
+  },
+  {
+    imageId: 'savvy_ninpo_down_the_ladder',
+    image: require('./savvy_ninpo_down_the_ladder.png'),
+  },
+  {
+    imageId: 'savvy_ninpo_stepping_down',
+    image: require('./savvy_ninpo_stepping_down.png'),
+  },
+  {
+    imageId: 'savvy_ninpo_into_my_office',
+    image: require('./savvy_ninpo_into_my_office.png'),
+  },
+  {
+    imageId: 'savvy_ninpo_safety_net',
+    image: require('./savvy_ninpo_safety_net.png'),
+  },
+  {
+    imageId: 'savvy_ninpo_tax_write_off',
+    image: require('./savvy_ninpo_tax_write_off.png'),
+  },
+  {
+    imageId: 'resshou',
+    image: require('./resshou.png'),
+  },
+  {
+    imageId: 'dead_stock_ninpo_firesale_1',
+    image: require('./dead_stock_ninpo_firesale_1.png'),
+  },
+//  {
+//    imageId: 'dead_stock_ninpo_firesale_2',
+//    image: require('./dead_stock_ninpo_firesale_2.png'),
+//  },
+  {
+    imageId: 'business_ultimate_ninpo_all_hands_1',
+    image: require('./business_ultimate_ninpo_all_hands_1.png'),
+  },
+//  {
+//    imageId: 'business_ultimate_ninpo_all_hands_2',
+//    image: require('./business_ultimate_ninpo_all_hands_2.png'),
+//  },
+  {
+    imageId: 'summoner_the_basilisk',
+    image: require('./summoner_the_basilisk.png'),
+  },
 ]
