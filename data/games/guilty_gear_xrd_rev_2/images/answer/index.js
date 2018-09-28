22bddad105486cb3a8f72cd3b42ab4e0248b7f8d
Alex Chvatal <achvatal@redhat.com>
2018-09-28 10:54:34 -0400

    add universal moves for guilty gear xrd rev 2
    closes #91

--- data/games/guilty_gear_xrd_rev_2/images/answer/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/answer/index.js
@@ -1,146 +1,158 @@
 export default [
   {
-    imageId: '5P',
-    image: require('./5P.png'),
+    "imageId": "summoner_the_basilisk",
+    "image": require('./summoner_the_basilisk.png')
   },
   {
-    imageId: '5K',
-    image: require('./5K.png'),
+    "imageId": "5K",
+    "image": require('./5K.png')
   },
   {
-    imageId: 'c_S',
-    image: require('./c_S.png'),
+    "imageId": "2K",
+    "image": require('./2K.png')
   },
   {
-    imageId: 'f_S',
-    image: require('./f_S.png'),
+    "imageId": "savvy_ninpo_into_my_office",
+    "image": require('./savvy_ninpo_into_my_office.png')
   },
   {
-    imageId: '5H',
-    image: require('./5H.png'),
+    "imageId": "2P",
+    "image": require('./2P.png')
   },
   {
-    imageId: '5D',
-    image: require('./5D.png'),
+    "imageId": "j_P",
+    "image": require('./j_P.png')
   },
   {
-    imageId: '6P',
-    image: require('./6P.png'),
+    "imageId": "j_K",
+    "image": require('./j_K.png')
   },
   {
-    imageId: '6K',
-    image: require('./6K.png'),
+    "imageId": "2D",
+    "image": require('./2D.png')
   },
   {
-    imageId: '6H',
-    image: require('./6H.png'),
+    "imageId": "j_H",
+    "image": require('./j_H.png')
   },
   {
-    imageId: '2P',
-    image: require('./2P.png'),
+    "imageId": "5P",
+    "image": require('./5P.png')
   },
   {
-    imageId: '2K',
-    image: require('./2K.png'),
+    "imageId": "business_ultimate_ninpo_all_hands_1",
+    "image": require('./business_ultimate_ninpo_all_hands_1.png')
   },
   {
-    imageId: '2S',
-    image: require('./2S.png'),
+    "imageId": "c_S",
+    "image": require('./c_S.png')
   },
   {
-    imageId: '2H',
-    image: require('./2H.png'),
+    "imageId": "j_S",
+    "image": require('./j_S.png')
   },
   {
-    imageId: '2D',
-    image: require('./2D.png'),
+    "imageId": "dead_stock_ninpo_firesale_1",
+    "image": require('./dead_stock_ninpo_firesale_1.png')
   },
   {
-    imageId: 'j_P',
-    image: require('./j_P.png'),
+    "imageId": "j_D",
+    "image": require('./j_D.png')
   },
   {
-    imageId: 'j_K',
-    image: require('./j_K.png'),
+    "imageId": "resshou",
+    "image": require('./resshou.png')
   },
   {
-    imageId: 'j_S',
-    image: require('./j_S.png'),
+    "imageId": "Ground_Throw",
+    "image": require('./Ground_Throw.png')
   },
   {
-    imageId: 'j_H',
-    image: require('./j_H.png'),
+    "imageId": "6H",
+    "image": require('./6H.png')
   },
   {
-    imageId: 'j_D',
-    image: require('./j_D.png'),
+    "imageId": "business_ultimate_ninpo_all_hands_2",
+    "image": require('./business_ultimate_ninpo_all_hands_2.png')
   },
   {
-    imageId: 'business_ninpo_caltrops',
-    image: require('./business_ninpo_caltrops.png'),
+    "imageId": "dead_stock_ninpo_firesale_2",
+    "image": require('./dead_stock_ninpo_firesale_2.png')
   },
   {
-    imageId: 'business_ninpo_under_the_rug',
-    image: require('./business_ninpo_under_the_rug.png'),
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
   },
   {
-    imageId: 'business_ninpo_under_the_bus',
-    image: require('./business_ninpo_under_the_bus.png'),
+    "imageId": "5H",
+    "image": require('./5H.png')
   },
   {
-    imageId: 'savvy_ninpo_request_for_approval',
-    image: require('./savvy_ninpo_request_for_approval.png'),
-  },
-//  {
-//    imageId: 'savvy_ninpo_seal_of_approval',
-//    image: require('./savvy_ninpo_seal_of_approval.png'),
-//  },
-  {
-    imageId: 'savvy_ninpo_data_logging',
-    image: require('./savvy_ninpo_data_logging.png'),
+    "imageId": "savvy_ninpo_seal_of_approval",
+    "image": require('./savvy_ninpo_seal_of_approval.png')
   },
   {
-    imageId: 'savvy_ninpo_down_the_ladder',
-    image: require('./savvy_ninpo_down_the_ladder.png'),
+    "imageId": "business_ninpo_under_the_rug",
+    "image": require('./business_ninpo_under_the_rug.png')
   },
   {
-    imageId: 'savvy_ninpo_stepping_down',
-    image: require('./savvy_ninpo_stepping_down.png'),
+    "imageId": "6P",
+    "image": require('./6P.png')
   },
   {
-    imageId: 'savvy_ninpo_into_my_office',
-    image: require('./savvy_ninpo_into_my_office.png'),
+    "imageId": "5D",
+    "image": require('./5D.png')
   },
   {
-    imageId: 'savvy_ninpo_safety_net',
-    image: require('./savvy_ninpo_safety_net.png'),
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
   },
   {
-    imageId: 'savvy_ninpo_tax_write_off',
-    image: require('./savvy_ninpo_tax_write_off.png'),
+    "imageId": "f_S",
+    "image": require('./f_S.png')
   },
   {
-    imageId: 'resshou',
-    image: require('./resshou.png'),
+    "imageId": "6K",
+    "image": require('./6K.png')
   },
   {
-    imageId: 'dead_stock_ninpo_firesale_1',
-    image: require('./dead_stock_ninpo_firesale_1.png'),
+    "imageId": "2H",
+    "image": require('./2H.png')
   },
-//  {
-//    imageId: 'dead_stock_ninpo_firesale_2',
-//    image: require('./dead_stock_ninpo_firesale_2.png'),
-//  },
   {
-    imageId: 'business_ultimate_ninpo_all_hands_1',
-    image: require('./business_ultimate_ninpo_all_hands_1.png'),
+    "imageId": "savvy_ninpo_down_the_ladder",
+    "image": require('./savvy_ninpo_down_the_ladder.png')
   },
-//  {
-//    imageId: 'business_ultimate_ninpo_all_hands_2',
-//    image: require('./business_ultimate_ninpo_all_hands_2.png'),
-//  },
   {
-    imageId: 'summoner_the_basilisk',
-    image: require('./summoner_the_basilisk.png'),
+    "imageId": "savvy_ninpo_safety_net",
+    "image": require('./savvy_ninpo_safety_net.png')
   },
+  {
+    "imageId": "business_ninpo_under_the_bus",
+    "image": require('./business_ninpo_under_the_bus.png')
+  },
+  {
+    "imageId": "savvy_ninpo_stepping_down",
+    "image": require('./savvy_ninpo_stepping_down.png')
+  },
+  {
+    "imageId": "savvy_ninpo_tax_write_off",
+    "image": require('./savvy_ninpo_tax_write_off.png')
+  },
+  {
+    "imageId": "savvy_ninpo_data_logging",
+    "image": require('./savvy_ninpo_data_logging.png')
+  },
+  {
+    "imageId": "savvy_ninpo_request_for_approval",
+    "image": require('./savvy_ninpo_request_for_approval.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "business_ninpo_caltrops",
+    "image": require('./business_ninpo_caltrops.png')
+  }
 ]
--- data/games/guilty_gear_xrd_rev_2/images/baiken/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/baiken/index.js
@@ -1,150 +1,162 @@
 export default [
   {
-    "imageId": "5P",
-    "image": require('./5P.png')
-  },
-  {
-    "imageId": "5K",
-    "image": require('./5K.png')
-  },
-  {
-    "imageId": "cS",
-    "image": require('./cS.png')
-  },
-  {
-    "imageId": "fS",
-    "image": require('./fS.png')
-  },
-  {
-    "imageId": "5H",
-    "image": require('./5H.png')
-  },
-  {
-    "imageId": "5D",
-    "image": require('./5D.png')
-  },
-  {
-    "imageId": "6P",
-    "image": require('./6P.png')
-  },
-  {
-    "imageId": "6K",
-    "image": require('./6K.png')
-  },
-  {
-    "imageId": "6H",
-    "image": require('./6H.png')
-  },
-  {
-    "imageId": "2P",
-    "image": require('./2P.png')
-  },
-  {
-    "imageId": "2K",
-    "image": require('./2K.png')
-  },
-  {
-    "imageId": "2S",
-    "image": require('./2S.png')
-  },
-  {
-    "imageId": "2H",
-    "image": require('./2H.png')
-  },
-  {
-    "imageId": "2D",
-    "image": require('./2D.png')
+    "imageId": "Himarawi",
+    "image": require('./Himarawi.png')
   },
   {
     "imageId": "jP",
     "image": require('./jP.png')
   },
   {
-    "imageId": "jK",
-    "image": require('./jK.png')
-  },
-  {
-    "imageId": "jS",
-    "image": require('./jS.png')
-  },
-  {
-    "imageId": "jH",
-    "image": require('./jH.png')
-  },
-  {
-    "imageId": "jD",
-    "image": require('./jD.png')
-  },
-  {
-    "imageId": "Tatami_Gaeshi",
-    "image": require('./Tatami_Gaeshi.png')
-  },
-  {
-    "imageId": "Azami",
-    "image": require('./Azami.png')
-  },
-  {
-    "imageId": "Suzuran",
-    "image": require('./Suzuran.png')
-  },
-  {
-    "imageId": "Kuchinashi",
-    "image": require('./Kuchinashi.png')
-  },
-  {
-    "imageId": "Mawarikomi",
-    "image": require('./Mawarikomi.png')
-  },
-  {
-    "imageId": "Sakura",
-    "image": require('./Sakura.png')
+    "imageId": "5K",
+    "image": require('./5K.png')
   },
   {
     "imageId": "Rokkonsogi",
     "image": require('./Rokkonsogi.png')
   },
   {
-    "imageId": "Yashagatana",
-    "image": require('./Yashagatana.png')
+    "imageId": "Kuchinashi",
+    "image": require('./Kuchinashi.png')
   },
   {
-    "imageId": "Air_Azami",
-    "image": require('./Air_Azami.png')
+    "imageId": "2K",
+    "image": require('./2K.png')
   },
   {
-    "imageId": "Tsubaki",
-    "image": require('./Tsubaki.png')
+    "imageId": "fS",
+    "image": require('./fS.png')
   },
   {
-    "imageId": "Kikyou",
-    "image": require('./Kikyou.png')
-  },
-  {
-    "imageId": "Kabari",
-    "image": require('./Kabari.png')
-  },
-  {
-    "imageId": "Himarawi",
-    "image": require('./Himarawi.png')
-  },
-  {
-    "imageId": "Tetsuzansen",
-    "image": require('./Tetsuzansen.png')
-  },
-  {
-    "imageId": "Youzansen",
-    "image": require('./Youzansen.png')
-  },
-  {
-    "imageId": "Tsurane_Zanzuwatashi",
-    "image": require('./Tsurane_Zanzuwatashi.png')
+    "imageId": "2P",
+    "image": require('./2P.png')
   },
   {
     "imageId": "Metsudo_Kushoudou",
     "image": require('./Metsudo_Kushoudou.png')
   },
   {
+    "imageId": "Youzansen",
+    "image": require('./Youzansen.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "Kabari",
+    "image": require('./Kabari.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "Ground_Throw",
+    "image": require('./Ground_Throw.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Sakura",
+    "image": require('./Sakura.png')
+  },
+  {
+    "imageId": "Tsurane_Zanzuwatashi",
+    "image": require('./Tsurane_Zanzuwatashi.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "Yashagatana",
+    "image": require('./Yashagatana.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "Suzuran",
+    "image": require('./Suzuran.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "Azami",
+    "image": require('./Azami.png')
+  },
+  {
+    "imageId": "Tetsuzansen",
+    "image": require('./Tetsuzansen.png')
+  },
+  {
+    "imageId": "Mawarikomi",
+    "image": require('./Mawarikomi.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "Kikyou",
+    "image": require('./Kikyou.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Tatami_Gaeshi",
+    "image": require('./Tatami_Gaeshi.png')
+  },
+  {
+    "imageId": "Tsubaki",
+    "image": require('./Tsubaki.png')
+  },
+  {
     "imageId": "Garyo_Tensei",
     "image": require('./Garyo_Tensei.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "Air_Azami",
+    "image": require('./Air_Azami.png')
   }
 ]
