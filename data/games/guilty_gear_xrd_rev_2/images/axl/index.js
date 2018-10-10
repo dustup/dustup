d7f01fa0a60d566231e5e36df576d6836540663a
Adam Price <komidore64@gmail.com>
2018-10-09 20:00:32 -0400

    GGXrd-R2 - axl images

--- data/games/guilty_gear_xrd_rev_2/images/axl/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/axl/index.js
@@ -1,6 +1,170 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "j_6P",
+    "image": require('./j_6P.png')
+  },
+  {
+    "imageId": "thunder_shadow_chain",
+    "image": require('./thunder_shadow_chain.png')
+  },
+  {
+    "imageId": "heaven_can_wait_1",
+    "image": require('./heaven_can_wait_1.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "heaven_can_wait_2",
+    "image": require('./heaven_can_wait_2.png')
+  },
+  {
+    "imageId": "sparrowhawk_stance_1",
+    "image": require('./sparrowhawk_stance_1.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "sickle_flash",
+    "image": require('./sickle_flash.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "sickle_storm",
+    "image": require('./sickle_storm.png')
+  },
+  {
+    "imageId": "melody_chain",
+    "image": require('./melody_chain.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "spindle_spinner_1",
+    "image": require('./spindle_spinner_1.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "spindle_spinner_2",
+    "image": require('./spindle_spinner_2.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "sparrowhawk_stance_2",
+    "image": require('./sparrowhawk_stance_2.png')
+  },
+  {
+    "imageId": "shark_strike",
+    "image": require('./shark_strike.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "spinning_chain_strike",
+    "image": require('./spinning_chain_strike.png')
+  },
+  {
+    "imageId": "artemis_hunter",
+    "image": require('./artemis_hunter.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "axl_bomber",
+    "image": require('./axl_bomber.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "3P",
+    "image": require('./3P.png')
+  },
+  {
+    "imageId": "amphora_conflagration_2",
+    "image": require('./amphora_conflagration_2.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "amphora_conflagration_1",
+    "image": require('./amphora_conflagration_1.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "j_6K",
+    "image": require('./j_6K.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "j_2S",
+    "image": require('./j_2S.png')
+  }
 ]
