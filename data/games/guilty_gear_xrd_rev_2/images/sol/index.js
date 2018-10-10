785c9fc4e924b5b90efa0313b1bf5de7836bbe65
Adam Price <komidore64@gmail.com>
2018-10-10 09:31:22 -0400

    GGXrd-R2 - sol images (#125)
    refs #87

--- data/games/guilty_gear_xrd_rev_2/images/sol/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/sol/index.js
@@ -1,6 +1,178 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "wild_throw_1",
+    "image": require('./wild_throw_1.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "bandit_revolver",
+    "image": require('./bandit_revolver.png')
+  },
+  {
+    "imageId": "tyrant_rave_ver_beta_2",
+    "image": require('./tyrant_rave_ver_beta_2.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "wild_throw_2",
+    "image": require('./wild_throw_2.png')
+  },
+  {
+    "imageId": "break_1",
+    "image": require('./break_1.png')
+  },
+  {
+    "imageId": "dragon_install_1",
+    "image": require('./dragon_install_1.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "riot_stamp_2",
+    "image": require('./riot_stamp_2.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "knockdown",
+    "image": require('./knockdown.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "branding_breach_2",
+    "image": require('./branding_breach_2.png')
+  },
+  {
+    "imageId": "break_2",
+    "image": require('./break_2.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "dragon_install_2",
+    "image": require('./dragon_install_2.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "volcanic_viper",
+    "image": require('./volcanic_viper.png')
+  },
+  {
+    "imageId": "ground_viper_1",
+    "image": require('./ground_viper_1.png')
+  },
+  {
+    "imageId": "p_b_b_2",
+    "image": require('./p_b_b_2.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "branding_breach_1",
+    "image": require('./branding_breach_1.png')
+  },
+  {
+    "imageId": "fafnir",
+    "image": require('./fafnir.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "bandit_bringer",
+    "image": require('./bandit_bringer.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "gun_flame",
+    "image": require('./gun_flame.png')
+  },
+  {
+    "imageId": "gun_flame_feint",
+    "image": require('./gun_flame_feint.png')
+  },
+  {
+    "imageId": "p_b_b_1",
+    "image": require('./p_b_b_1.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "riot_stamp_1",
+    "image": require('./riot_stamp_1.png')
+  },
+  {
+    "imageId": "tyrant_rave_ver_beta_1",
+    "image": require('./tyrant_rave_ver_beta_1.png')
+  },
+  {
+    "imageId": "ground_viper_2",
+    "image": require('./ground_viper_2.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  }
 ]
