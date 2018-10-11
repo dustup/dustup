c74c7cfb417a8bec2408a54efb0ea89e9d068f31
Alex Chvatal <yith@yuggoth.space>
2018-10-11 11:38:12 -0400

    add images for leo from xrd (#127)

--- data/games/guilty_gear_xrd_rev_2/images/leo/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/leo/index.js
@@ -1,6 +1,186 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "Graviert_Wurde_H",
+    "image": require('./Graviert_Wurde_H.png')
+  },
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "Leidenschaft_Dirigen_1",
+    "image": require('./Leidenschaft_Dirigen_1.png')
+  },
+  {
+    "imageId": "Dritt",
+    "image": require('./Dritt.png')
+  },
+  {
+    "imageId": "Kahn_Schild",
+    "image": require('./Kahn_Schild.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "Siegesparade",
+    "image": require('./Siegesparade.png')
+  },
+  {
+    "imageId": "Zweit",
+    "image": require('./Zweit.png')
+  },
+  {
+    "imageId": "Windrad_des_Weltraum_2",
+    "image": require('./Windrad_des_Weltraum_2.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "Dead_Angle",
+    "image": require('./Dead_Angle.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "btP",
+    "image": require('./btP.png')
+  },
+  {
+    "imageId": "Blitzschlag",
+    "image": require('./Blitzschlag.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Windrad_des_Weltraum_1",
+    "image": require('./Windrad_des_Weltraum_1.png')
+  },
+  {
+    "imageId": "fS_P",
+    "image": require('./fS_P.png')
+  },
+  {
+    "imageId": "Stahl_Wirbel_1",
+    "image": require('./Stahl_Wirbel_1.png')
+  },
+  {
+    "imageId": "btH",
+    "image": require('./btH.png')
+  },
+  {
+    "imageId": "5H_P",
+    "image": require('./5H_P.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "btS",
+    "image": require('./btS.png')
+  },
+  {
+    "imageId": "Brynhildr_Stance",
+    "image": require('./Brynhildr_Stance.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "Erst",
+    "image": require('./Erst.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "Graviert_Wurde_S",
+    "image": require('./Graviert_Wurde_S.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Eisen_Sturm",
+    "image": require('./Eisen_Sturm.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Stahl_Wirbel_2",
+    "image": require('./Stahl_Wirbel_2.png')
+  },
+  {
+    "imageId": "btK",
+    "image": require('./btK.png')
+  },
+  {
+    "imageId": "Windrad_des_Weltraum_3",
+    "image": require('./Windrad_des_Weltraum_3.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "Leidenschaft_Dirigen_2",
+    "image": require('./Leidenschaft_Dirigen_2.png')
+  }
 ]
