c664b4d356473b45201afb13c3f7206f018cb7b6
Alex Chvatal <yith@yuggoth.space>
2018-10-12 10:46:33 -0400

    add images for sin from guilty gear (#129)
    also adds his calorie mechanics

--- data/games/guilty_gear_xrd_rev_2/images/sin/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/sin/index.js
@@ -1,6 +1,146 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "Elk_Hunt",
+    "image": require('./Elk_Hunt.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "Im_Sure_Ill_Hit_Something",
+    "image": require('./Im_Sure_Ill_Hit_Something.png')
+  },
+  {
+    "imageId": "Still_Growing",
+    "image": require('./Still_Growing.png')
+  },
+  {
+    "imageId": "j6H",
+    "image": require('./j6H.png')
+  },
+  {
+    "imageId": "Beak_Driver_air",
+    "image": require('./Beak_Driver_air.png')
+  },
+  {
+    "imageId": "Voltic_Deign",
+    "image": require('./Voltic_Deign.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "Bull_Bash",
+    "image": require('./Bull_Bash.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Leap",
+    "image": require('./Leap.png')
+  },
+  {
+    "imageId": "Raiden_1",
+    "image": require('./Raiden_1.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "RTL",
+    "image": require('./RTL.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "Raiden_2",
+    "image": require('./Raiden_2.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Vulture_Seize",
+    "image": require('./Vulture_Seize.png')
+  },
+  {
+    "imageId": "3K",
+    "image": require('./3K.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Beak_Driver",
+    "image": require('./Beak_Driver.png')
+  },
+  {
+    "imageId": "Hawk_Baker",
+    "image": require('./Hawk_Baker.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  }
 ]
