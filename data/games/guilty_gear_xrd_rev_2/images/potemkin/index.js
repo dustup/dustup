758de3cc0497f96ec7b9579c641ebb588ca65053
Adam Price <komidore64@gmail.com>
2018-10-11 20:07:56 -0400

    GGXrd-R2 - potemkin images

--- data/games/guilty_gear_xrd_rev_2/images/potemkin/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/potemkin/index.js
@@ -1,6 +1,166 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "infernal_tour_1",
+    "image": require('./infernal_tour_1.png')
+  },
+  {
+    "imageId": "trishula",
+    "image": require('./trishula.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "potemkin_buster_2",
+    "image": require('./potemkin_buster_2.png')
+  },
+  {
+    "imageId": "i_c_p_m",
+    "image": require('./i_c_p_m.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "f_d_b_1",
+    "image": require('./f_d_b_1.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "heat_extend",
+    "image": require('./heat_extend.png')
+  },
+  {
+    "imageId": "heavenly_potemkin_buster_2",
+    "image": require('./heavenly_potemkin_buster_2.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "slide_head",
+    "image": require('./slide_head.png')
+  },
+  {
+    "imageId": "infernal_tour_3",
+    "image": require('./infernal_tour_3.png')
+  },
+  {
+    "imageId": "potemkin_buster_1",
+    "image": require('./potemkin_buster_1.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "megafist",
+    "image": require('./megafist.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "giganter_ex",
+    "image": require('./giganter_ex.png')
+  },
+  {
+    "imageId": "infernal_tour_2",
+    "image": require('./infernal_tour_2.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "heat_knuckle",
+    "image": require('./heat_knuckle.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "heavenly_potemkin_buster_1",
+    "image": require('./heavenly_potemkin_buster_1.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "hammer_fall",
+    "image": require('./hammer_fall.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "heavenly_potemkin_buster_3",
+    "image": require('./heavenly_potemkin_buster_3.png')
+  },
+  {
+    "imageId": "f_d_b_2",
+    "image": require('./f_d_b_2.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "gigantic_blid_ex",
+    "image": require('./gigantic_blid_ex.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  }
 ]
