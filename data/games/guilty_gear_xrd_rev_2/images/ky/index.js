68feaaee4ae27657187291d3b4dac1f0bbe10d1c
Adam Price <komidore64@gmail.com>
2018-10-14 18:20:29 -0400

    GGXrd-R2 - ky images

--- data/games/guilty_gear_xrd_rev_2/images/ky/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/ky/index.js
@@ -1,6 +1,154 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "split_ciel",
+    "image": require('./split_ciel.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "air_stun_edge",
+    "image": require('./air_stun_edge.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "greed_sever",
+    "image": require('./greed_sever.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "ride_the_lightning",
+    "image": require('./ride_the_lightning.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "vapor_thrust",
+    "image": require('./vapor_thrust.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "stun_edge_fortified",
+    "image": require('./stun_edge_fortified.png')
+  },
+  {
+    "imageId": "air_stun_edge_fortified",
+    "image": require('./air_stun_edge_fortified.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "stun_dipper",
+    "image": require('./stun_dipper.png')
+  },
+  {
+    "imageId": "charged_stun_edge_fortified",
+    "image": require('./charged_stun_edge_fortified.png')
+  },
+  {
+    "imageId": "sacred_edge_fortified",
+    "image": require('./sacred_edge_fortified.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "charged_stun_edge",
+    "image": require('./charged_stun_edge.png')
+  },
+  {
+    "imageId": "3H",
+    "image": require('./3H.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "stun_edge",
+    "image": require('./stun_edge.png')
+  },
+  {
+    "imageId": "rising_force_2",
+    "image": require('./rising_force_2.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "sacred_edge",
+    "image": require('./sacred_edge.png')
+  },
+  {
+    "imageId": "rising_force_1",
+    "image": require('./rising_force_1.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  }
 ]
