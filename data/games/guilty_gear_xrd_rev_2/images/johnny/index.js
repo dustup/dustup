a79fc0e749d4554216d83339b94784eb8727c54b
Adam Price <komidore64@gmail.com>
2018-10-15 17:50:55 -0400

    GGXrd-R2 - johnny images (#131)
    refs #87

--- data/games/guilty_gear_xrd_rev_2/images/johnny/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/johnny/index.js
@@ -1,6 +1,170 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "zweihander",
+    "image": require('./zweihander.png')
+  },
+  {
+    "imageId": "air_low_mist_finer",
+    "image": require('./air_low_mist_finer.png')
+  },
+  {
+    "imageId": "joker_trick_3",
+    "image": require('./joker_trick_3.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "treasure_hunt_1",
+    "image": require('./treasure_hunt_1.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "thats_my_name_1",
+    "image": require('./thats_my_name_1.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "mid_mist_finer",
+    "image": require('./mid_mist_finer.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "air_mid_mist_finer",
+    "image": require('./air_mid_mist_finer.png')
+  },
+  {
+    "imageId": "high_mist_finer",
+    "image": require('./high_mist_finer.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "baccus_sigh",
+    "image": require('./baccus_sigh.png')
+  },
+  {
+    "imageId": "joker_trick_2",
+    "image": require('./joker_trick_2.png')
+  },
+  {
+    "imageId": "air_mist_stance",
+    "image": require('./air_mist_stance.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "low_mist_finer",
+    "image": require('./low_mist_finer.png')
+  },
+  {
+    "imageId": "thats_my_name_2",
+    "image": require('./thats_my_name_2.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "joker_trick_1",
+    "image": require('./joker_trick_1.png')
+  },
+  {
+    "imageId": "air_high_mist_finer",
+    "image": require('./air_high_mist_finer.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "glitter_is_gold",
+    "image": require('./glitter_is_gold.png')
+  },
+  {
+    "imageId": "zweihander_attack",
+    "image": require('./zweihander_attack.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "joker_trick_4",
+    "image": require('./joker_trick_4.png')
+  },
+  {
+    "imageId": "treasure_hunt_2",
+    "image": require('./treasure_hunt_2.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "mist_stance",
+    "image": require('./mist_stance.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  }
 ]
