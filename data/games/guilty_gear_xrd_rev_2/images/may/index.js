8e8f9509fe6e38a0db181f3c0a9c2bb4e9bd8dda
Alex Chvatal <yith@yuggoth.space>
2018-10-17 18:04:02 -0400

    add images for may from guilty gear (#133)
    also adds mechanics for her riding mr dolphin and bouncing on beach
    balls

--- data/games/guilty_gear_xrd_rev_2/images/may/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/may/index.js
@@ -1,6 +1,166 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "Ultimate_Spinning_Whirlwind_2",
+    "image": require('./Ultimate_Spinning_Whirlwind_2.png')
+  },
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "Overhead_Kiss_1",
+    "image": require('./Overhead_Kiss_1.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "Farewell_3",
+    "image": require('./Farewell_3.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "Ensenga",
+    "image": require('./Ensenga.png')
+  },
+  {
+    "imageId": "Farewell_2",
+    "image": require('./Farewell_2.png')
+  },
+  {
+    "imageId": "Mr_Dolphin_Vertical",
+    "image": require('./Mr_Dolphin_Vertical.png')
+  },
+  {
+    "imageId": "j2H",
+    "image": require('./j2H.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "Deluxe_Goshogawara_Bomber",
+    "image": require('./Deluxe_Goshogawara_Bomber.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Overhead_Kiss_2",
+    "image": require('./Overhead_Kiss_2.png')
+  },
+  {
+    "imageId": "Great_Yamada_Attack_2",
+    "image": require('./Great_Yamada_Attack_2.png')
+  },
+  {
+    "imageId": "Mr_Dolphin_Horizontal",
+    "image": require('./Mr_Dolphin_Horizontal.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "Applause_for_the_Victim",
+    "image": require('./Applause_for_the_Victim.png')
+  },
+  {
+    "imageId": "Farewell_1",
+    "image": require('./Farewell_1.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "Dont_Miss_It_1",
+    "image": require('./Dont_Miss_It_1.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Dont_Miss_It_2",
+    "image": require('./Dont_Miss_It_2.png')
+  },
+  {
+    "imageId": "Ultimate_Whiner",
+    "image": require('./Ultimate_Whiner.png')
+  },
+  {
+    "imageId": "3K",
+    "image": require('./3K.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Great_Yamada_Attack_1",
+    "image": require('./Great_Yamada_Attack_1.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "Ultimate_Spinning_Whirlwind_1",
+    "image": require('./Ultimate_Spinning_Whirlwind_1.png')
+  }
 ]
