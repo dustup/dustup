11850f412dc5027da980d1dd5466930206c952ba
Alex Chvatal <achvatal@redhat.com>
2018-10-19 13:43:39 -0400

    add images for chipp from guilty gear

--- data/games/guilty_gear_xrd_rev_2/images/chipp/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/chipp/index.js
@@ -1,6 +1,194 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "Gamma_Blade_1",
+    "image": require('./Gamma_Blade_1.png')
+  },
+  {
+    "imageId": "j2K",
+    "image": require('./j2K.png')
+  },
+  {
+    "imageId": "Alpha_Blade_Plus",
+    "image": require('./Alpha_Blade_Plus.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "Genrouzan_2",
+    "image": require('./Genrouzan_2.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Resshou",
+    "image": require('./Resshou.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "Rokusai",
+    "image": require('./Rokusai.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "Shinkirou",
+    "image": require('./Shinkirou.png')
+  },
+  {
+    "imageId": "Banki_Mesai",
+    "image": require('./Banki_Mesai.png')
+  },
+  {
+    "imageId": "Tsuyoshishiki_Meisei",
+    "image": require('./Tsuyoshishiki_Meisei.png')
+  },
+  {
+    "imageId": "Wall_Cling",
+    "image": require('./Wall_Cling.png')
+  },
+  {
+    "imageId": "Beta_Blade",
+    "image": require('./Beta_Blade.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "Raisetsudan_2",
+    "image": require('./Raisetsudan_2.png')
+  },
+  {
+    "imageId": "Shuriken",
+    "image": require('./Shuriken.png')
+  },
+  {
+    "imageId": "Ryuu_Yanagi",
+    "image": require('./Ryuu_Yanagi.png')
+  },
+  {
+    "imageId": "Gamma_Blade_2",
+    "image": require('./Gamma_Blade_2.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Genrouzan_1",
+    "image": require('./Genrouzan_1.png')
+  },
+  {
+    "imageId": "Wall_Ground_Throw",
+    "image": require('./Wall_Ground_Throw.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Wall_Air_Throw",
+    "image": require('./Wall_Air_Throw.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "Tsuyoshishiki_Teni",
+    "image": require('./Tsuyoshishiki_Teni.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "Raisetsudan_1",
+    "image": require('./Raisetsudan_1.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "Zansei_Rouga_1",
+    "image": require('./Zansei_Rouga_1.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Wall_Alpha",
+    "image": require('./Wall_Alpha.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "Wall_Kunai",
+    "image": require('./Wall_Kunai.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "Senshuu",
+    "image": require('./Senshuu.png')
+  },
+  {
+    "imageId": "Zansei_Rouga_2",
+    "image": require('./Zansei_Rouga_2.png')
+  },
+  {
+    "imageId": "Alpha_Blade",
+    "image": require('./Alpha_Blade.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  }
 ]
