3b406e36e45bc153871c854d0be4d9e905627bd5
Adam Price <komidore64@gmail.com>
2018-10-21 11:18:00 -0400

    GGXrd-R2 - bedman images

--- data/games/guilty_gear_xrd_rev_2/images/bedman/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/bedman/index.js
@@ -1,6 +1,142 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "sinusoidal_helios_2",
+    "image": require('./sinusoidal_helios_2.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "task_b",
+    "image": require('./task_b.png')
+  },
+  {
+    "imageId": "deja_vu_2",
+    "image": require('./deja_vu_2.png')
+  },
+  {
+    "imageId": "crouch_walk",
+    "image": require('./crouch_walk.png')
+  },
+  {
+    "imageId": "hemi_jack",
+    "image": require('./hemi_jack.png')
+  },
+  {
+    "imageId": "task_a",
+    "image": require('./task_a.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "task_c",
+    "image": require('./task_c.png')
+  },
+  {
+    "imageId": "2H_diagram",
+    "image": require('./2H_diagram.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "deja_vu_1",
+    "image": require('./deja_vu_1.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "task_a_dash",
+    "image": require('./task_a_dash.png')
+  },
+  {
+    "imageId": "theatre_of_pain_1",
+    "image": require('./theatre_of_pain_1.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "sinusoidal_helios_1",
+    "image": require('./sinusoidal_helios_1.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "dash",
+    "image": require('./dash.png')
+  },
+  {
+    "imageId": "air_movement",
+    "image": require('./air_movement.png')
+  },
+  {
+    "imageId": "theatre_of_pain_2",
+    "image": require('./theatre_of_pain_2.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  }
 ]
