6b71ccf2f52dc80530555701b1b6b7d9ec18428f
Adam Price <komidore64@gmail.com>
2018-10-22 12:23:36 -0400

    GGXrd-R2 - dizzy images (#136)
    refs #87

--- data/games/guilty_gear_xrd_rev_2/images/dizzy/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/dizzy/index.js
@@ -1,6 +1,166 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "we_talked_a_lot_together",
+    "image": require('./we_talked_a_lot_together.png')
+  },
+  {
+    "imageId": "gamma_ray",
+    "image": require('./gamma_ray.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "for_putting_out_the_light",
+    "image": require('./for_putting_out_the_light.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "what_happens_when_im_too_alone",
+    "image": require('./what_happens_when_im_too_alone.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "we_fought_a_lot_together_1",
+    "image": require('./we_fought_a_lot_together_1.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "dont_be_overprotective",
+    "image": require('./dont_be_overprotective.png')
+  },
+  {
+    "imageId": "please_leave_me_alone",
+    "image": require('./please_leave_me_alone.png')
+  },
+  {
+    "imageId": "i_used_this_to_pick_fruit_1",
+    "image": require('./i_used_this_to_pick_fruit_1.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "for_roasting_chestnuts_2",
+    "image": require('./for_roasting_chestnuts_2.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "emotional_gamma_ray_1",
+    "image": require('./emotional_gamma_ray_1.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "for_searing_cod",
+    "image": require('./for_searing_cod.png')
+  },
+  {
+    "imageId": "we_fought_a_lot_together_2",
+    "image": require('./we_fought_a_lot_together_2.png')
+  },
+  {
+    "imageId": "imperial_ray",
+    "image": require('./imperial_ray.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "emotional_gamma_ray_2",
+    "image": require('./emotional_gamma_ray_2.png')
+  },
+  {
+    "imageId": "i_used_this_to_catch_fish",
+    "image": require('./i_used_this_to_catch_fish.png')
+  },
+  {
+    "imageId": "for_roasting_chestnuts_1",
+    "image": require('./for_roasting_chestnuts_1.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "the_light_was_so_small_in_the_beginning",
+    "image": require('./the_light_was_so_small_in_the_beginning.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "4S",
+    "image": require('./4S.png')
+  },
+  {
+    "imageId": "i_used_this_to_pick_fruit_2",
+    "image": require('./i_used_this_to_pick_fruit_2.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "emotional_gamma_ray_3",
+    "image": require('./emotional_gamma_ray_3.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  }
 ]
