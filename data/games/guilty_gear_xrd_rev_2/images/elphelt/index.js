e378478f601df03b91a82613272badb2141e0529
Alex Chvatal <yith@yuggoth.space>
2018-10-23 11:21:52 -0400

    add images for elphelt from guilty gear xrd rev 2 (#137)

--- data/games/guilty_gear_xrd_rev_2/images/elphelt/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/elphelt/index.js
@@ -1,6 +1,158 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "Miss_Travailler_Demolish",
+    "image": require('./Miss_Travailler_Demolish.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "Miss_Travailler_Roll",
+    "image": require('./Miss_Travailler_Roll.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "Judge_Better_Half_2",
+    "image": require('./Judge_Better_Half_2.png')
+  },
+  {
+    "imageId": "Judge_Better_Half_1",
+    "image": require('./Judge_Better_Half_1.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "Miss_Confire_reticule",
+    "image": require('./Miss_Confire_reticule.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "Magnum_Wedding_3",
+    "image": require('./Magnum_Wedding_3.png')
+  },
+  {
+    "imageId": "Bridal_Express_air",
+    "image": require('./Bridal_Express_air.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "Magnum_Wedding_1",
+    "image": require('./Magnum_Wedding_1.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Miss_Travailler_Thrust",
+    "image": require('./Miss_Travailler_Thrust.png')
+  },
+  {
+    "imageId": "Miss_Travailler_stance",
+    "image": require('./Miss_Travailler_stance.png')
+  },
+  {
+    "imageId": "Bridal_Express",
+    "image": require('./Bridal_Express.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "Pineberry_1",
+    "image": require('./Pineberry_1.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "Miss_Travailler_Launch",
+    "image": require('./Miss_Travailler_Launch.png')
+  },
+  {
+    "imageId": "Miss_Confire_stance",
+    "image": require('./Miss_Confire_stance.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Pineberry_2",
+    "image": require('./Pineberry_2.png')
+  },
+  {
+    "imageId": "Miss_Travailler_Fire",
+    "image": require('./Miss_Travailler_Fire.png')
+  },
+  {
+    "imageId": "Genoverse",
+    "image": require('./Genoverse.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "Magnum_Wedding_2",
+    "image": require('./Magnum_Wedding_2.png')
+  }
 ]
