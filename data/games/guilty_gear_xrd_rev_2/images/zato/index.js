87b77ebf078fe6fcda8878b79c6089d77a344dc9
Adam Price <komidore64@gmail.com>
2018-10-25 15:11:16 -0400

    GGXrd-R2 - zato images

--- data/games/guilty_gear_xrd_rev_2/images/zato/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/zato/index.js
@@ -1,6 +1,182 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "summon_eddie",
+    "image": require('./summon_eddie.png')
+  },
+  {
+    "imageId": "traversing_attack",
+    "image": require('./traversing_attack.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "guatemala_magnifica_2",
+    "image": require('./guatemala_magnifica_2.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "shadow_gallery_2",
+    "image": require('./shadow_gallery_2.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "great_white_2",
+    "image": require('./great_white_2.png')
+  },
+  {
+    "imageId": "shadow_gallery_1",
+    "image": require('./shadow_gallery_1.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "drill_special",
+    "image": require('./drill_special.png')
+  },
+  {
+    "imageId": "anti_air_attack",
+    "image": require('./anti_air_attack.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "damned_fang_2",
+    "image": require('./damned_fang_2.png')
+  },
+  {
+    "imageId": "invite_hell",
+    "image": require('./invite_hell.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "guatemala_magnifica_3",
+    "image": require('./guatemala_magnifica_3.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "dead_mans_hand",
+    "image": require('./dead_mans_hand.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "guatemala_magnifica_1",
+    "image": require('./guatemala_magnifica_1.png')
+  },
+  {
+    "imageId": "executor",
+    "image": require('./executor.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "shadow_puddle",
+    "image": require('./shadow_puddle.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "drunkard_shade",
+    "image": require('./drunkard_shade.png')
+  },
+  {
+    "imageId": "amorphous",
+    "image": require('./amorphous.png')
+  },
+  {
+    "imageId": "small_attack",
+    "image": require('./small_attack.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "damned_fang_1",
+    "image": require('./damned_fang_1.png')
+  },
+  {
+    "imageId": "shadow_puddle_shadow_summon",
+    "image": require('./shadow_puddle_shadow_summon.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "break_the_law",
+    "image": require('./break_the_law.png')
+  },
+  {
+    "imageId": "great_white_1",
+    "image": require('./great_white_1.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "flight",
+    "image": require('./flight.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  }
 ]
