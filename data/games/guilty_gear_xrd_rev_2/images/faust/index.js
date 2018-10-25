0549aac33ff5ba303fe06254fc93ef17ba1de178
Alex Chvatal <achvatal@redhat.com>
2018-10-25 09:13:16 -0400

    add images for faust from guilty gear

--- data/games/guilty_gear_xrd_rev_2/images/faust/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/faust/index.js
@@ -1,6 +1,170 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "Hello_Again",
+    "image": require('./Hello_Again.png')
+  },
+  {
+    "imageId": "Stimulating_Fists_2",
+    "image": require('./Stimulating_Fists_2.png')
+  },
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "Stimulating_Fists_1",
+    "image": require('./Stimulating_Fists_1.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "What_Could_This_Be",
+    "image": require('./What_Could_This_Be.png')
+  },
+  {
+    "imageId": "W-W-What_Could_This_Be",
+    "image": require('./W-W-What_Could_This_Be.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "Love",
+    "image": require('./Love.png')
+  },
+  {
+    "imageId": "Pogo_Weird",
+    "image": require('./Pogo_Weird.png')
+  },
+  {
+    "imageId": "Hello",
+    "image": require('./Hello.png')
+  },
+  {
+    "imageId": "Hack_n_Slash",
+    "image": require('./Hack_n_Slash.png')
+  },
+  {
+    "imageId": "Going_My_Way",
+    "image": require('./Going_My_Way.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "Rerere_Thrust",
+    "image": require('./Rerere_Thrust.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "From_The_1",
+    "image": require('./From_The_1.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Doctor_Copter",
+    "image": require('./Doctor_Copter.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "j2K",
+    "image": require('./j2K.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "This_Is_Me_2",
+    "image": require('./This_Is_Me_2.png')
+  },
+  {
+    "imageId": "From_The_2",
+    "image": require('./From_The_2.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "Pogo_Flowers",
+    "image": require('./Pogo_Flowers.png')
+  },
+  {
+    "imageId": "This_Is_Me_1",
+    "image": require('./This_Is_Me_1.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Spear_Point_Centripetal_Dance",
+    "image": require('./Spear_Point_Centripetal_Dance.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Pogo_Movement",
+    "image": require('./Pogo_Movement.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "Stimulating_Fists_3",
+    "image": require('./Stimulating_Fists_3.png')
+  }
 ]
