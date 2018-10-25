cd4fec8949361d7e079a4e6c24be3b979f20c720
Alex Chvatal <achvatal@redhat.com>
2018-10-25 16:55:15 -0400

    add images for i-no from guilty gear

--- data/games/guilty_gear_xrd_rev_2/images/i-no/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/i-no/index.js
@@ -1,6 +1,146 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "Sterilization_Method_2",
+    "image": require('./Sterilization_Method_2.png')
+  },
+  {
+    "imageId": "Longing_Desperation_2",
+    "image": require('./Longing_Desperation_2.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "Ultimate_Fortissimo_2",
+    "image": require('./Ultimate_Fortissimo_2.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "6S",
+    "image": require('./6S.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "Horizontal_Chemical_Love_followup",
+    "image": require('./Horizontal_Chemical_Love_followup.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "Megalomania_2",
+    "image": require('./Megalomania_2.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "Sultry_Performance",
+    "image": require('./Sultry_Performance.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Sterilization_Method_1",
+    "image": require('./Sterilization_Method_1.png')
+  },
+  {
+    "imageId": "Stroke_the_Big_Tree",
+    "image": require('./Stroke_the_Big_Tree.png')
+  },
+  {
+    "imageId": "Ultimate_Fortissimo_1",
+    "image": require('./Ultimate_Fortissimo_1.png')
+  },
+  {
+    "imageId": "Longing_Desperation_1",
+    "image": require('./Longing_Desperation_1.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Horizontal_Chemical_Love",
+    "image": require('./Horizontal_Chemical_Love.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Vertical_Chemical_Love",
+    "image": require('./Vertical_Chemical_Love.png')
+  },
+  {
+    "imageId": "Antidepressant_Scale",
+    "image": require('./Antidepressant_Scale.png')
+  },
+  {
+    "imageId": "Megalomania_1",
+    "image": require('./Megalomania_1.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  }
 ]
