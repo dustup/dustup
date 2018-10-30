ad11ea95c2c95bafe0eb6d9f346efa0855518647
Adam Price <komidore64@gmail.com>
2018-10-30 07:10:27 -0400

    GGXrd-R2 - slayer images

--- data/games/guilty_gear_xrd_rev_2/images/slayer/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/slayer/index.js
@@ -1,6 +1,158 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "helter_skelter",
+    "image": require('./helter_skelter.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "all_dead_1",
+    "image": require('./all_dead_1.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "its_late",
+    "image": require('./its_late.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "pilebunker",
+    "image": require('./pilebunker.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "mappa_punch",
+    "image": require('./mappa_punch.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "crosswise_heel",
+    "image": require('./crosswise_heel.png')
+  },
+  {
+    "imageId": "under_pressure",
+    "image": require('./under_pressure.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "up_close_and_dandy",
+    "image": require('./up_close_and_dandy.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "j_2K",
+    "image": require('./j_2K.png')
+  },
+  {
+    "imageId": "dandy_step",
+    "image": require('./dandy_step.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "footloose_journey",
+    "image": require('./footloose_journey.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "eternal_wings",
+    "image": require('./eternal_wings.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "bloodsucking_universe_2",
+    "image": require('./bloodsucking_universe_2.png')
+  },
+  {
+    "imageId": "undertow",
+    "image": require('./undertow.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "all_dead_2",
+    "image": require('./all_dead_2.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "dead_on_time",
+    "image": require('./dead_on_time.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "bloodsucking_universe_1",
+    "image": require('./bloodsucking_universe_1.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  }
 ]
