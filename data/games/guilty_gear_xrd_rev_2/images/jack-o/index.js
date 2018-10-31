720a425144f9d2f369e990f2425e7ca932450f21
Alex Chvatal <achvatal@redhat.com>
2018-10-31 09:10:18 -0400

    add images for jack-o from xrd

--- data/games/guilty_gear_xrd_rev_2/images/jack-o/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/jack-o/index.js
@@ -1,6 +1,170 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "Elysion_Driver_1",
+    "image": require('./Elysion_Driver_1.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "Calvados_1",
+    "image": require('./Calvados_1.png')
+  },
+  {
+    "imageId": "Aegis_Field",
+    "image": require('./Aegis_Field.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Ghost_Put_Away",
+    "image": require('./Ghost_Put_Away.png')
+  },
+  {
+    "imageId": "Hunger_of_Dopoulos",
+    "image": require('./Hunger_of_Dopoulos.png')
+  },
+  {
+    "imageId": "Organ_Deployment",
+    "image": require('./Organ_Deployment.png')
+  },
+  {
+    "imageId": "I_Want_Out_3",
+    "image": require('./I_Want_Out_3.png')
+  },
+  {
+    "imageId": "I_Want_Out_2",
+    "image": require('./I_Want_Out_2.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "Air_Zest",
+    "image": require('./Air_Zest.png')
+  },
+  {
+    "imageId": "Elysion_Driver_2",
+    "image": require('./Elysion_Driver_2.png')
+  },
+  {
+    "imageId": "Ghost_Place",
+    "image": require('./Ghost_Place.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Remove_the_Chain_of_Chiron",
+    "image": require('./Remove_the_Chain_of_Chiron.png')
+  },
+  {
+    "imageId": "3H",
+    "image": require('./3H.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Ghost_Throw",
+    "image": require('./Ghost_Throw.png')
+  },
+  {
+    "imageId": "Ghost_Pick_Up",
+    "image": require('./Ghost_Pick_Up.png')
+  },
+  {
+    "imageId": "Forever_Elysion_Driver_2",
+    "image": require('./Forever_Elysion_Driver_2.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "Explode",
+    "image": require('./Explode.png')
+  },
+  {
+    "imageId": "Clock_Up",
+    "image": require('./Clock_Up.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "Zest",
+    "image": require('./Zest.png')
+  },
+  {
+    "imageId": "Ghost_Set",
+    "image": require('./Ghost_Set.png')
+  },
+  {
+    "imageId": "Recover_All",
+    "image": require('./Recover_All.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Forever_Elysion_Driver_1",
+    "image": require('./Forever_Elysion_Driver_1.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "Calvados_2",
+    "image": require('./Calvados_2.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  }
 ]
