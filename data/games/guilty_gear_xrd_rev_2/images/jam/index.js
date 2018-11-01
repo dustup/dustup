c55a2cf8ace7925b7915598a71991ef0e20302d1
Alex Chvatal <achvatal@redhat.com>
2018-11-01 11:01:39 -0400

    add images for jam from xrd

--- data/games/guilty_gear_xrd_rev_2/images/jam/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/jam/index.js
@@ -1,6 +1,178 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "Phoenix_2",
+    "image": require('./Phoenix_2.png')
+  },
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "Phoenix_1",
+    "image": require('./Phoenix_1.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "Bakushuu",
+    "image": require('./Bakushuu.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "Hyappo_Shinshou",
+    "image": require('./Hyappo_Shinshou.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "Senri_Shinshou",
+    "image": require('./Senri_Shinshou.png')
+  },
+  {
+    "imageId": "Dowanga",
+    "image": require('./Dowanga.png')
+  },
+  {
+    "imageId": "Youeikyaku",
+    "image": require('./Youeikyaku.png')
+  },
+  {
+    "imageId": "Goushao",
+    "image": require('./Goushao.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "Ryuujin",
+    "image": require('./Ryuujin.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "Gekirin",
+    "image": require('./Gekirin.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Renhoukyaku",
+    "image": require('./Renhoukyaku.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "Asanagi_no_Kokyuu_1",
+    "image": require('./Asanagi_no_Kokyuu_1.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "Gasenkotsu_3",
+    "image": require('./Gasenkotsu_3.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "Gasenkotsu_2",
+    "image": require('./Gasenkotsu_2.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "Mawarikomi",
+    "image": require('./Mawarikomi.png')
+  },
+  {
+    "imageId": "Dragon_2",
+    "image": require('./Dragon_2.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Hamonkyaku",
+    "image": require('./Hamonkyaku.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "Dragon_1",
+    "image": require('./Dragon_1.png')
+  },
+  {
+    "imageId": "Asanagi_no_Kokyuu_2",
+    "image": require('./Asanagi_no_Kokyuu_2.png')
+  },
+  {
+    "imageId": "Zekkei",
+    "image": require('./Zekkei.png')
+  },
+  {
+    "imageId": "Hochifu",
+    "image": require('./Hochifu.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "Kenroukaku",
+    "image": require('./Kenroukaku.png')
+  }
 ]
