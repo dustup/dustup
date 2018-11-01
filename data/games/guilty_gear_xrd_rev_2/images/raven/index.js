357c4bf12a32c2411698d10e0bf4c37ebe6ad24d
Adam Price <komidore64@gmail.com>
2018-11-01 12:48:01 -0400

    GGXrd-R2 - raven images (#144)
    refs #87

--- data/games/guilty_gear_xrd_rev_2/images/raven/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/raven/index.js
@@ -1,6 +1,158 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "sehnsucht_3",
+    "image": require('./sehnsucht_3.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "4P",
+    "image": require('./4P.png')
+  },
+  {
+    "imageId": "scharf_kugel",
+    "image": require('./scharf_kugel.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "gebrechlich_licht",
+    "image": require('./gebrechlich_licht.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "wachen_zweig_1",
+    "image": require('./wachen_zweig_1.png')
+  },
+  {
+    "imageId": "give_it_to_me_here_2",
+    "image": require('./give_it_to_me_here_2.png')
+  },
+  {
+    "imageId": "verzweifelt_2",
+    "image": require('./verzweifelt_2.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "getreuer_1",
+    "image": require('./getreuer_1.png')
+  },
+  {
+    "imageId": "glide",
+    "image": require('./glide.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "schmerz_berg",
+    "image": require('./schmerz_berg.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "give_it_to_me_here_1",
+    "image": require('./give_it_to_me_here_1.png')
+  },
+  {
+    "imageId": "j_2K",
+    "image": require('./j_2K.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "sehnsucht_2",
+    "image": require('./sehnsucht_2.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "getreuer_2",
+    "image": require('./getreuer_2.png')
+  },
+  {
+    "imageId": "sehnsucht_1",
+    "image": require('./sehnsucht_1.png')
+  },
+  {
+    "imageId": "wachen_zweig_2",
+    "image": require('./wachen_zweig_2.png')
+  },
+  {
+    "imageId": "verzweifelt_1",
+    "image": require('./verzweifelt_1.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "grausam_impuls",
+    "image": require('./grausam_impuls.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  }
 ]
