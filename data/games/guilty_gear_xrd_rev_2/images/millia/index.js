fd6d75c334a3e88ad13c8fefc4fb5141534bf9cd
Alex Chvatal <achvatal@redhat.com>
2018-11-05 10:15:32 -0500

    add images for millia rage from xrd

--- data/games/guilty_gear_xrd_rev_2/images/millia/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/millia/index.js
@@ -1,6 +1,166 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "Tandem_Top_1",
+    "image": require('./Tandem_Top_1.png')
+  },
+  {
+    "imageId": "Lust_Shaker",
+    "image": require('./Lust_Shaker.png')
+  },
+  {
+    "imageId": "Tandem_Top_2",
+    "image": require('./Tandem_Top_2.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "fS",
+    "image": require('./fS.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "Iron_Maiden_2",
+    "image": require('./Iron_Maiden_2.png')
+  },
+  {
+    "imageId": "Silent_Force_2",
+    "image": require('./Silent_Force_2.png')
+  },
+  {
+    "imageId": "Chroming_Rose_1",
+    "image": require('./Chroming_Rose_1.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "Winger_1",
+    "image": require('./Winger_1.png')
+  },
+  {
+    "imageId": "Chroming_Rose_2",
+    "image": require('./Chroming_Rose_2.png')
+  },
+  {
+    "imageId": "Silent_Force_1",
+    "image": require('./Silent_Force_1.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Forward_Roll",
+    "image": require('./Forward_Roll.png')
+  },
+  {
+    "imageId": "Winger_2",
+    "image": require('./Winger_2.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "jH",
+    "image": require('./jH.png')
+  },
+  {
+    "imageId": "Turbo_Fall",
+    "image": require('./Turbo_Fall.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Digitalis",
+    "image": require('./Digitalis.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "Bad_Moon",
+    "image": require('./Bad_Moon.png')
+  },
+  {
+    "imageId": "Iron_Maiden_3",
+    "image": require('./Iron_Maiden_3.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "Secret_Garden",
+    "image": require('./Secret_Garden.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Iron_Savior",
+    "image": require('./Iron_Savior.png')
+  },
+  {
+    "imageId": "Emerald_Rain",
+    "image": require('./Emerald_Rain.png')
+  },
+  {
+    "imageId": "Iron_Maiden_1",
+    "image": require('./Iron_Maiden_1.png')
+  },
+  {
+    "imageId": "jS",
+    "image": require('./jS.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  }
 ]
