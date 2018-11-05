0463561c2d81d7f6edceeb67e80b031cc4952df6
Adam Price <komidore64@gmail.com>
2018-11-05 08:39:27 -0500

    GGXrd-R2 - kum images (#147)
    refs #87

--- data/games/guilty_gear_xrd_rev_2/images/kum/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/kum/index.js
@@ -1,6 +1,146 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "j_H",
+    "image": require('./j_H.png')
+  },
+  {
+    "imageId": "ground_throw",
+    "image": require('./ground_throw.png')
+  },
+  {
+    "imageId": "j_K",
+    "image": require('./j_K.png')
+  },
+  {
+    "imageId": "sundering_chord_4",
+    "image": require('./sundering_chord_4.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "blitz_attack",
+    "image": require('./blitz_attack.png')
+  },
+  {
+    "imageId": "air_falcon_dive",
+    "image": require('./air_falcon_dive.png')
+  },
+  {
+    "imageId": "j_S",
+    "image": require('./j_S.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "four_tigers_sword_2",
+    "image": require('./four_tigers_sword_2.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "j_P",
+    "image": require('./j_P.png')
+  },
+  {
+    "imageId": "j_2K",
+    "image": require('./j_2K.png')
+  },
+  {
+    "imageId": "5H",
+    "image": require('./5H.png')
+  },
+  {
+    "imageId": "tuning_ball",
+    "image": require('./tuning_ball.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "falcon_dive",
+    "image": require('./falcon_dive.png')
+  },
+  {
+    "imageId": "celestial_tuning_ball",
+    "image": require('./celestial_tuning_ball.png')
+  },
+  {
+    "imageId": "sundering_chord_3",
+    "image": require('./sundering_chord_3.png')
+  },
+  {
+    "imageId": "f_S",
+    "image": require('./f_S.png')
+  },
+  {
+    "imageId": "sundering_chord_1",
+    "image": require('./sundering_chord_1.png')
+  },
+  {
+    "imageId": "sundering_chord_2",
+    "image": require('./sundering_chord_2.png')
+  },
+  {
+    "imageId": "j_D",
+    "image": require('./j_D.png')
+  },
+  {
+    "imageId": "c_S",
+    "image": require('./c_S.png')
+  },
+  {
+    "imageId": "enlightened_3000_palm_strike",
+    "image": require('./enlightened_3000_palm_strike.png')
+  },
+  {
+    "imageId": "four_tigers_sword_1",
+    "image": require('./four_tigers_sword_1.png')
+  },
+  {
+    "imageId": "air_throw",
+    "image": require('./air_throw.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "four_tigers_sword_reverse",
+    "image": require('./four_tigers_sword_reverse.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "falcon_dive_reverse",
+    "image": require('./falcon_dive_reverse.png')
+  }
 ]
