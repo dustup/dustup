f5f0fec75b6f88d95ecd9e9e1b71879b5851b1f9
Alex Chvatal <yith@yuggoth.space>
2018-11-06 08:24:35 -0500

    add ramlethal images for guilty gear (#149)
    holy goddamn is ram's moveset crazy

--- data/games/guilty_gear_xrd_rev_2/images/ramlethal/index.js
+++ data/games/guilty_gear_xrd_rev_2/images/ramlethal/index.js
@@ -1,6 +1,230 @@
 export default [
-  // {
-  //   "imageId": "5A",
-  //   "image": require('./5A.png')
-  // }
+  {
+    "imageId": "Explode_1",
+    "image": require('./Explode_1.png')
+  },
+  {
+    "imageId": "Flama_Cargo",
+    "image": require('./Flama_Cargo.png')
+  },
+  {
+    "imageId": "Retrieve_Giant_Sword",
+    "image": require('./Retrieve_Giant_Sword.png')
+  },
+  {
+    "imageId": "Calvados",
+    "image": require('./Calvados.png')
+  },
+  {
+    "imageId": "jP",
+    "image": require('./jP.png')
+  },
+  {
+    "imageId": "5K",
+    "image": require('./5K.png')
+  },
+  {
+    "imageId": "jH_with_sword",
+    "image": require('./jH_with_sword.png')
+  },
+  {
+    "imageId": "2K",
+    "image": require('./2K.png')
+  },
+  {
+    "imageId": "jH_no_sword",
+    "image": require('./jH_no_sword.png')
+  },
+  {
+    "imageId": "fS_no_sword",
+    "image": require('./fS_no_sword.png')
+  },
+  {
+    "imageId": "2P",
+    "image": require('./2P.png')
+  },
+  {
+    "imageId": "6S",
+    "image": require('./6S.png')
+  },
+  {
+    "imageId": "2D",
+    "image": require('./2D.png')
+  },
+  {
+    "imageId": "5P",
+    "image": require('./5P.png')
+  },
+  {
+    "imageId": "5PPK",
+    "image": require('./5PPK.png')
+  },
+  {
+    "imageId": "4K",
+    "image": require('./4K.png')
+  },
+  {
+    "imageId": "jD",
+    "image": require('./jD.png')
+  },
+  {
+    "imageId": "jK",
+    "image": require('./jK.png')
+  },
+  {
+    "imageId": "6H",
+    "image": require('./6H.png')
+  },
+  {
+    "imageId": "Daruo_enhanced",
+    "image": require('./Daruo_enhanced.png')
+  },
+  {
+    "imageId": "Animo_Estingi_2",
+    "image": require('./Animo_Estingi_2.png')
+  },
+  {
+    "imageId": "5H_with_sword",
+    "image": require('./5H_with_sword.png')
+  },
+  {
+    "imageId": "Animo_Estingi_3",
+    "image": require('./Animo_Estingi_3.png')
+  },
+  {
+    "imageId": "2KK",
+    "image": require('./2KK.png')
+  },
+  {
+    "imageId": "Trance",
+    "image": require('./Trance.png')
+  },
+  {
+    "imageId": "Blitz_Attack",
+    "image": require('./Blitz_Attack.png')
+  },
+  {
+    "imageId": "2S_with_sword",
+    "image": require('./2S_with_sword.png')
+  },
+  {
+    "imageId": "2PP",
+    "image": require('./2PP.png')
+  },
+  {
+    "imageId": "6P",
+    "image": require('./6P.png')
+  },
+  {
+    "imageId": "5D",
+    "image": require('./5D.png')
+  },
+  {
+    "imageId": "Air_Throw",
+    "image": require('./Air_Throw.png')
+  },
+  {
+    "imageId": "cS",
+    "image": require('./cS.png')
+  },
+  {
+    "imageId": "jS_with_sword",
+    "image": require('./jS_with_sword.png')
+  },
+  {
+    "imageId": "fS_with_sword",
+    "image": require('./fS_with_sword.png')
+  },
+  {
+    "imageId": "2PK",
+    "image": require('./2PK.png')
+  },
+  {
+    "imageId": "5KPP",
+    "image": require('./5KPP.png')
+  },
+  {
+    "imageId": "5KKK",
+    "image": require('./5KKK.png')
+  },
+  {
+    "imageId": "5PP",
+    "image": require('./5PP.png')
+  },
+  {
+    "imageId": "6K",
+    "image": require('./6K.png')
+  },
+  {
+    "imageId": "2H",
+    "image": require('./2H.png')
+  },
+  {
+    "imageId": "Daruo",
+    "image": require('./Daruo.png')
+  },
+  {
+    "imageId": "jS_no_sword",
+    "image": require('./jS_no_sword.png')
+  },
+  {
+    "imageId": "j8D",
+    "image": require('./j8D.png')
+  },
+  {
+    "imageId": "5PPP",
+    "image": require('./5PPP.png')
+  },
+  {
+    "imageId": "5H_no_sword",
+    "image": require('./5H_no_sword.png')
+  },
+  {
+    "imageId": "Explode_2",
+    "image": require('./Explode_2.png')
+  },
+  {
+    "imageId": "3K",
+    "image": require('./3K.png')
+  },
+  {
+    "imageId": "Cassius",
+    "image": require('./Cassius.png')
+  },
+  {
+    "imageId": "Silda_Detruo",
+    "image": require('./Silda_Detruo.png')
+  },
+  {
+    "imageId": "Throw",
+    "image": require('./Throw.png')
+  },
+  {
+    "imageId": "5PK",
+    "image": require('./5PK.png')
+  },
+  {
+    "imageId": "Marteli",
+    "image": require('./Marteli.png')
+  },
+  {
+    "imageId": "j2D",
+    "image": require('./j2D.png')
+  },
+  {
+    "imageId": "2S",
+    "image": require('./2S.png')
+  },
+  {
+    "imageId": "2PPK",
+    "image": require('./2PPK.png')
+  },
+  {
+    "imageId": "Forpeli",
+    "image": require('./Forpeli.png')
+  },
+  {
+    "imageId": "2H_with_sword",
+    "image": require('./2H_with_sword.png')
+  }
 ]
